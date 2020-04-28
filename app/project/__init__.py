502d5c6e3eb1c6fcf9dc46bd7719561c19698fb4
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-04-28 12:15:47 +0200

    Postgres Setup: - instantiate the db - Add test model (users-db)

--- app/project/__init__.py
+++ app/project/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify
 import os
 import sys
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 #init the app
 app = Flask(__name__)
@@ -8,6 +10,22 @@ app = Flask(__name__)
 #set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+# model user to test the db
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 # test to ensure the proper config was loaded
 #print(app.config, file=sys.stderr)
