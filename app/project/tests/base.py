db9459cf8f4624010e50cb03d64c0ef433e2da51
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-04-28 16:15:09 +0200

    Test Setup: - Add base test class

---
+++ app/project/tests/base.py
@@ -0,0 +1,15 @@
+from flask_testing import TestCase
+from project import app, db
+
+class BaseTestCase(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.TestingConfig')
+        return app
+
+    def set_up(self):
+        db.create_all()
+        db.session.commit()
+
+    def tear_down(self):
+        db.session.remove()
+        db.drop_all()
