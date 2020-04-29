89e368c0e8888ccd05087318074c1720f06d6c4e
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-04-29 16:19:33 +0200

    add endpoints

--- app/project/urls.py
+++ app/project/urls.py
@@ -1,6 +1,19 @@
 from project.api.ping import Ping
+from project.api.platformList import PlatformList
+from project.api.platformDetail import PlatformDetail
+from project.api.platformRelationship import PlatformRelationship
+from project.api.deviceList import DeviceList
+from project.api.deviceDetail import DeviceDetail
+from project.api.deviceRelationship import DeviceRelationship
+
 
 
 def Create_endpoints(api):
 
     api.route(Ping, 'test_connection', '/ping')
+    api.route(PlatformList, 'platform_list', '/platforms')
+    api.route(PlatformDetail, 'platform_detail', '/platforms/<int:id>', '/devices/<int:device_id>/platform')
+    api.route(PlatformRelationship, 'platform_devices', '/platforms/<int:id>/relationships/devices')
+    api.route(DeviceList, 'devices_list', '/devices', '/platforms/<int:id>/devices')
+    api.route(DeviceDetail, 'devices_detail', '/devices/<int:id>')
+    api.route(DeviceRelationship, 'device_platform', '/devices/<int:id>/relationships/platform')
