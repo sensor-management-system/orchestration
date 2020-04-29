985affedd048e07ca7c03d9aad98a44758eda538
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-04-29 09:46:51 +0200

    move ping class to it own file

---
+++ app/project/api/ping.py
@@ -0,0 +1,15 @@
+from flask_rest_jsonapi import ResourceList
+
+
+
+
+class Ping(ResourceList):
+    def get(self):
+        response = {
+            'status': 'success',
+            'message': 'Hello Sensor!'
+        }
+        return response
+
+
+
