0e51ac244f8dd411f7129f80c3c7910a839c4751
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-05-04 10:33:08 +0200

    Add properties schema

---
+++ app/project/api/schemas/propertiesSchema.py
@@ -0,0 +1,41 @@
+from marshmallow_jsonapi import fields
+from marshmallow_jsonapi.flask import Schema, Relationship
+
+
+class PropertiesSchema(Schema):
+    """
+    This class create a schema for a device. Every attribute in the schema going to expose through the api.
+    DeviceSchema has an attribute named “deviceURN” that is the result of concatenation manufacturer, model,
+    type and serialNumber.
+    It uses library called marshmallow-jsonapi that fit the JSONAPI 1.0 specification and provides Flask integration.
+
+    """
+
+    class Meta:
+        type_ = 'properties'
+        self_view = 'properties_detail'
+        self_view_kwargs = {'id': '<id>'}
+
+    id = fields.Integer(as_string=True, dump_only=True)
+    accuracy = fields.Str(allow_none=True)
+    label = fields.Str(allow_none=True)
+    unit = fields.Str(allow_none=True)
+    Compartment = fields.Str(required=True)
+    measuringRangeMin = fields.Float(as_string=True, required=True)
+    measuringRangeMax = fields.Float(as_string=True, required=True)
+    failureValue = fields.Float(as_string=True, required=True)
+    Variable = fields.Str(required=True)
+    model = fields.Str(required=True)
+    dualUse = fields.Str()
+    inventoryNumber = fields.Integer()
+    website = fields.Str(allow_none=True)
+    SamplingMedia = fields.Date(allow_none=True)
+    persistentIdentifier = fields.Integer()
+
+    device = Relationship(attribute='properties',
+                          self_view='properties_events',
+                          self_view_kwargs={'id': '<id>'},
+                          related_view='properties_detail',
+                          related_view_kwargs={'id': '<id>'},
+                          schema='PropertiesSchema',
+                          type_='properties')
