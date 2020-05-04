b2282c6ce555a3644d2ada35d1547fa6c634e448
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-05-04 10:12:23 +0200

    change user to contact model

---
+++ app/project/api/models/contact.py
@@ -0,0 +1,9 @@
+from project.api.models.baseModel import db
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    device_id = db.Column(db.Integer, db.ForeignKey('device.id'))
+    device = db.relationship('Device', backref=db.backref('contacts'))
