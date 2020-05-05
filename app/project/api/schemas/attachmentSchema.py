5836addded923c5e2dabc70e3cdb132cb3a0a577
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-05-05 16:04:50 +0200

    Add attachment schema

---
+++ app/project/api/schemas/attachmentSchema.py
@@ -0,0 +1,27 @@
+from marshmallow_jsonapi import fields
+from marshmallow_jsonapi.flask import Schema, Relationship
+
+
+class AttachmentSchema(Schema):
+    """
+    This class create a schema for a attachment. Every attribute in the schema going to expose through the api.
+    It uses library called marshmallow-jsonapi that fit the JSONAPI 1.0 specification and provides Flask integration.
+
+    """
+
+    class Meta:
+        type_ = 'attachment'
+        self_view = 'attachments_detail'
+        self_view_kwargs = {'id': '<id>'}
+
+    id = fields.Integer(as_string=True, dump_only=True)
+    label = fields.Str(allow_none=True)
+    src = fields.Str(required=True)
+
+    device = Relationship(attribute='device',
+                          self_view='device_attachments',
+                          self_view_kwargs={'id': '<id>'},
+                          related_view='devices_detail',
+                          related_view_kwargs={'id': '<id>'},
+                          schema='DeviceSchema',
+                          type_='device')
