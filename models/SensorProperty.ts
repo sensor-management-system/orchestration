ed0793d315bafed0bc92621f6bec03c4b62127ec
Marc Hanisch <mhanisch@gfz-potsdam.de>
2020-05-08 14:28:31 +0200

    adds Sensor models

---
+++ models/SensorProperty.ts
@@ -0,0 +1,108 @@
+export class MeasuringRange {
+  private _min: number | null = null
+  private _max: number | null = null
+
+  constructor (min: number | null = null, max: number | null = null) {
+    this.min = min
+    this.max = max
+  }
+
+  /*
+  static createFromJson (someObject: Object) : MeasuringRange {
+    for (const key in someObject) {
+      this[(key as keyof MeasuringRange)] = someObject[(key as keyof Object)]
+    }
+  }
+  */
+
+  get min (): number | null {
+    return this._min
+  }
+
+  set min (min: number | null) {
+    this._min = min
+  }
+
+  get max (): number | null {
+    return this._max
+  }
+
+  set max (max: number | null) {
+    this._max = max
+  }
+}
+
+export default class SensorProperty {
+  private _compartment: string = ''
+  private _unit: string = ''
+  private _accuracy: number | null = null
+  private _samplingMedia: string = ''
+  private _measuringRange: MeasuringRange = new MeasuringRange()
+  private _label: string = ''
+  private _variable: string = ''
+  private _failureValue: number | null = null
+
+  get compartment (): string {
+    return this._compartment
+  }
+
+  set compartment (compartment: string) {
+    this._compartment = compartment
+  }
+
+  get unit (): string {
+    return this._unit
+  }
+
+  set unit (unit: string) {
+    this._unit = unit
+  }
+
+  get accuracy (): number | null {
+    return this._accuracy
+  }
+
+  set accuracy (accuracy: number | null) {
+    this._accuracy = accuracy
+  }
+
+  get samplingMedia (): string {
+    return this._samplingMedia
+  }
+
+  set samplingMedia (samplingMedia: string) {
+    this._samplingMedia = samplingMedia
+  }
+
+  get measuringRange (): MeasuringRange {
+    return this._measuringRange
+  }
+
+  set measuringRange (measuringRange: MeasuringRange) {
+    this._measuringRange = measuringRange
+  }
+
+  get label (): string {
+    return this._label
+  }
+
+  set label (label: string) {
+    this._label = label
+  }
+
+  get variable (): string {
+    return this._variable
+  }
+
+  set variable (variable: string) {
+    this._variable = variable
+  }
+
+  get failureValue (): number | null {
+    return this._failureValue
+  }
+
+  set failureValue (failureValue: number | null) {
+    this._failureValue = failureValue
+  }
+}
