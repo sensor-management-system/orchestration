5a9d7e5123d0d07c007e067743f2b743b5efff2a
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2020-05-12 08:14:55 +0200

    Failing test case test

--- test/True.spec.js
+++ test/True.spec.js
@@ -3,6 +3,6 @@ import { mount } from '@vue/test-utils'
 /* This is a dummy test to check that the ci works. */
 describe('True', () => {
   test('true is true', () => {
-    expect(true).toBeTruthy()
+    expect(false).toBeTruthy()
   })
 })
