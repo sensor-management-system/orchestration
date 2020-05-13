76c3f7a963cda0a133fc3c16669d17f0f43358ae
Martin Abbrent <martin.abbrent@ufz.de>
2020-05-13 15:46:47 +0200

    refactoring data model

--- app/project/api/models/attachment.py
+++ app/project/api/models/attachment.py
@@ -3,7 +3,7 @@ from project.api.models.baseModel import db
 
 class Attachment(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    label = db.Column(db.String(128))
-    src = db.Column(db.String(128), nullable=False)
+    label = db.Column(db.String(256))
+    url = db.Column(db.String(1024), nullable=False)
     device_id = db.Column(db.Integer, db.ForeignKey('device.id'))
     device = db.relationship('Device', backref=db.backref('attachment'))
--- app/project/api/models/customfield.py
+++ app/project/api/models/customfield.py
@@ -1,9 +1,9 @@
 from project.api.models.baseModel import db
 
 
-class Field(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    key = db.Column(db.String(128))
-    value = db.Column(db.String(128))
+class CustomField(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(256))
+    value = db.Column(db.String(1024))
     device_id = db.Column(db.Integer, db.ForeignKey('device.id'))
     device = db.relationship('Device', backref=db.backref('field'))
