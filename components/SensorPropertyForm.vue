5663c7dbe3cea07678f0d2fe796fbe12235517d8
Marc Hanisch <mhanisch@gfz-potsdam.de>
2020-05-20 14:16:23 +0200

    implements component behaviour

--- components/SensorPropertyForm.vue
+++ components/SensorPropertyForm.vue
@@ -4,16 +4,22 @@
       <v-col cols="12" md="3">
         <v-select
           label="compartment"
+          :value="value.compartment"
+          @input="update('compartment', $event)"
         />
       </v-col>
       <v-col cols="12" md="3">
         <v-select
           label="unit"
+          :value="value.unit"
+          @input="update('unit', $event)"
         />
       </v-col>
       <v-col cols="12" md="3">
         <v-text-field
           label="accuracy"
+          :value="value.accuracy"
+          @input="update('accuracy', $event)"
         />
       </v-col>
     </v-row>
@@ -21,21 +27,29 @@
       <v-col cols="12" md="3">
         <v-select
           label="sampling media"
+          :value="value.samplingMedia"
+          @input="update('samplingMedia', $event)"
         />
       </v-col>
       <v-col cols="12" md="1">
         <v-text-field
           label="measuring range min"
+          :value="value.measuringRange.min"
+          @input="update('measuringRange.min', $event)"
         />
       </v-col>
       <v-col cols="12" md="1">
         <v-text-field
-          label="measuring range min"
+          label="measuring range max"
+          :value="value.measuringRange.max"
+          @input="update('measuringRange.max', $event)"
         />
       </v-col>
       <v-col cols="12" md="3" offset="1">
         <v-text-field
           label="label"
+          :value="value.label"
+          @input="update('label', $event)"
         />
       </v-col>
     </v-row>
@@ -43,11 +57,15 @@
       <v-col cols="12" md="3">
         <v-select
           label="variable"
+          :value="value.variable"
+          @input="update('variable', $event)"
         />
       </v-col>
       <v-col cols="12" md="3">
         <v-text-field
           label="failure value"
+          :value="value.failureValue"
+          @input="update('failureValue', $event)"
         />
       </v-col>
     </v-row>
@@ -59,7 +77,7 @@
  * @file provides a component for a sensor property
  * @author <marc.hanisch@gfz-potsdam.de>
  */
-import { Vue, Component, Prop, Watch } from 'nuxt-property-decorator'
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
 import { SensorProperty } from '../models/SensorProperty'
 
 /**
@@ -69,5 +87,31 @@ import { SensorProperty } from '../models/SensorProperty'
 @Component
 // @ts-ignore
 export default class SensorPropertyForm extends Vue {
+  @Prop({
+    default: () => new SensorProperty(),
+    required: true,
+    type: SensorProperty
+  })
+  // @ts-ignore
+  value!: SensorProperty
+
+  /**
+   * update the internal model at a given key
+   *
+   * @param {string} key - a path to the property to set
+   * @param {string} value - the value to set
+   * @fires SensorPropertyForm#input
+   */
+  update (key: string, value: string) {
+    const newObj: SensorProperty = SensorProperty.createFromObject(this.value)
+    newObj.setPath(key, value)
+
+    /**
+     * input event
+     * @event SensorPropertyForm#input
+     * @type SensorProperty
+     */
+    this.$emit('input', newObj)
+  }
 }
 </script>
