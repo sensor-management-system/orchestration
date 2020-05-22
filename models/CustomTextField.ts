da6c6f59b7cb9739e3ec65e54a932dce851522d3
Marc Hanisch <mhanisch@gfz-potsdam.de>
2020-05-22 12:26:54 +0200

    implements IPathSetter interface

--- models/CustomTextField.ts
+++ models/CustomTextField.ts
@@ -1,6 +1,48 @@
-export default class CustomTextField {
+import IPathSetter from './IPathSetter'
+
+export interface ICustomTextField {
+  key: string,
+  value: string
+}
+
+export class CustomTextField implements ICustomTextField, IPathSetter {
   private _key: string = ''
   private _value: string = ''
+
+  /**
+   * creates an instance from another object
+   *
+   * @static
+   * @param {ICustomTextField} someObject - the object from which the new instance is to be created
+   * @return {CustomTextField} the newly created instance
+   */
+  static createFromObject (someObject: ICustomTextField) : CustomTextField {
+    const newObject = new CustomTextField()
+
+    newObject.key = someObject.key
+    newObject.value = someObject.value
+
+    return newObject
+  }
+
+  /**
+   * sets the value at the given path
+   *
+   * @param {string} path - the path (property name) to the value
+   * @param {any} value - the value to set
+   */
+  setPath (path: string, value: any): void {
+    switch (path) {
+      case 'key':
+        this.key = String(value)
+        break
+      case 'value':
+        this.value = String(value)
+        break
+      default:
+        throw new TypeError('path ' + path + ' is not valid')
+    }
+  }
 
   get key (): string {
     return this._key
@@ -8,5 +50,13 @@ export default class CustomTextField {
 
   set key (key: string) {
     this._key = key
+  }
+
+  get value (): string {
+    return this._value
+  }
+
+  set value (value: string) {
+    this._value = value
   }
 }
