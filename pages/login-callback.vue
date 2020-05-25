1af45860f37af55ffed798f79b0323336bdc2c85
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-05-25 16:23:42 +0200

    Add callback Page

---
+++ pages/login-callback.vue
@@ -0,0 +1,29 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    import { mapActions } from 'vuex'
+
+    export default {
+        name: "login-callback",
+        fetch ({ store }) {
+            store.commit('increment')
+        },
+        methods: {
+            ...mapActions( 'auth',[
+                'handleSigninPopupCallback'
+            ])
+        },
+        created() {
+            this.handleSigninPopupCallback()
+                .catch((err) => {
+                    console.error(err) // Handle errors any way you want
+                })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
