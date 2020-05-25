3d1fbc90d0f0893e8976a945d0b300d0cca8f293
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-05-25 16:24:08 +0200

    Add logout Page

---
+++ pages/logout-callback.vue
@@ -0,0 +1,29 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    import {mapActions} from 'vuex'
+
+    export default {
+        name: "logout-callback",
+        fetch ({ store }) {
+            store.commit('increment')
+        },
+        methods: {
+            ...mapActions( 'auth',[
+                'handleSignoutPopupCallback'
+            ])
+        },
+        created() {
+            this.handleSignoutPopupCallback()
+                .catch((err) => {
+                    console.error(err) // Handle errors any way you want
+                })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
