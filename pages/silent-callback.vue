14097873986c4a4b1bc705326ce1546605f09d72
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-05-25 16:24:45 +0200

    Add silent callback Page

---
+++ pages/silent-callback.vue
@@ -0,0 +1,25 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    import {mapActions} from 'vuex'
+    export default {
+        name: "SilentRenewCallback",
+        methods: {
+            ...mapActions( 'auth',[
+                'handleSilentRenewCallback'
+            ])
+        },
+        created() {
+            this.handleSilentRenewCallback()
+                .catch((err) => {
+                    console.error(err) // Handle errors any way you want
+                })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
