c7441628dfb29299269b38ef3cd3c2239013e8c0
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-05-27 09:37:18 +0200

    Refactoring

--- app/project/api/ping.py
+++ app/project/api/ping.py
@@ -2,7 +2,14 @@ from flask_rest_jsonapi import ResourceList
 
 
 class Ping(ResourceList):
+    """
+    This class allow client to ping the API.
+    """
+
     def get(self):
+        """Using GET method to ping API
+        :return: response
+        """
         response = {
             'status': 'success',
             'message': 'Hello Sensor!'
