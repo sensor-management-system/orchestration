ec67f37b75fefefa4b77b4c1678470c9270cbdd0
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-06-02 11:06:09 +0200

    remove unnecessary comment add flask blueprint to app

--- app/project/config.py
+++ app/project/config.py
@@ -1,24 +1,24 @@
 import os
 
 
-class BaseConfig:  # pylint: disable=too-few-public-methods
+class BaseConfig:
     """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'top_secret'
 
 
-class DevelopmentConfig(BaseConfig):  # pylint: disable=too-few-public-methods
+class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
-class TestingConfig(BaseConfig):  # pylint: disable=too-few-public-methods
+class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
-class ProductionConfig(BaseConfig):  # pylint: disable=too-few-public-methods
+class ProductionConfig(BaseConfig):
     """Production configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
