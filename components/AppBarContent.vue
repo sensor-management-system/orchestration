e4646fbaed4c17fe64014b24cc992c7d21f43c4e
Marc Hanisch <mhanisch@gfz-potsdam.de>
2020-06-03 16:19:54 +0200

    adds components for the app-bar

---
+++ components/AppBarContent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="v-toolbar__content" style="width:100%">
+    <v-toolbar-title>{{ title }}</v-toolbar-title>
+    <v-spacer />
+    <v-btn v-if="!cancelBtnHidden" color="secondary" class="mr-1" :disabled="cancelBtnDisabled" @click="$nuxt.$emit('AppBarContent:cancel-button-click')">Cancel</v-btn>
+    <v-btn v-if="!saveBtnHidden" color="primary" :disabled="saveBtnDisabled" @click="$nuxt.$emit('AppBarContent:save-button-click')">Save</v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+/**
+ * @file provides a component with save and cancel buttons for the App-Bar
+ * @author <marc.hanisch@gfz-potsdam.de>
+ */
+import { Vue, Component } from 'nuxt-property-decorator'
+
+/**
+ * A class component to select persons
+ * @extends Vue
+ */
+@Component
+// @ts-ignore
+export default class AppBarContent extends Vue {
+  get title (): string {
+    return ''
+  }
+
+  private saveBtnHidden: boolean = true
+  private cancelBtnHidden: boolean = true
+  private saveBtnDisabled: boolean = false
+  private cancelBtnDisabled: boolean = false
+
+  created () {
+    this.$nuxt.$on('AppBarContent:save-button-hidden', (hidden: boolean) => {
+      this.saveBtnHidden = hidden
+    })
+    this.$nuxt.$on('AppBarContent:save-button-disabled', (disabled: boolean) => {
+      this.saveBtnDisabled = disabled
+    })
+    this.$nuxt.$on('AppBarContent:cancel-button-hidden', (hidden: boolean) => {
+      this.cancelBtnHidden = hidden
+    })
+    this.$nuxt.$on('AppBarContent:cancel-button-disabled', (disabled: boolean) => {
+      this.cancelBtnDisabled = disabled
+    })
+  }
+}
+</script>
---
+++ components/AppBarExtension.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-tabs
+    :value="activeTab"
+    @change="changeTab"
+    background-color="grey lighten-3"
+  >
+    <v-tab
+      v-for="(tab, index) in tabs"
+      :key="index"
+    >
+      {{ tab }}
+    </v-tab>
+  </v-tabs>
+</template>
+
+<script lang="ts">
+/**
+ * @file provides a component with save and cancel buttons for the App-Bar
+ * @author <marc.hanisch@gfz-potsdam.de>
+ */
+import { Vue, Component } from 'nuxt-property-decorator'
+
+/**
+ * A class component to select persons
+ * @extends Vue
+ */
+@Component
+// @ts-ignore
+export default class AppBarExtension extends Vue {
+  private activeTab: number = 0
+
+  get tabs (): String[] {
+    return [] as String[]
+  }
+
+  changeTab (tab: number) {
+    this.activeTab = tab
+    this.$nuxt.$emit('AppBarExtension:change', tab)
+  }
+}
+</script>
