31a2e74daeed48974246ad3698f5de49d78d82f1
Marc Hanisch <mhanisch@gfz-potsdam.de>
2020-06-17 14:47:57 +0200

    adds AttachmentList and AttachmentListItem component

---
+++ components/AttachmentList.vue
@@ -0,0 +1,136 @@
+<template>
+  <v-container>
+    <v-row v-if="!readonly">
+      <v-col cols="12" md="3">
+        <v-text-field
+          v-model="attachment.label"
+          label="Label"
+          required
+          class="required"
+        />
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-file-input accept="image/*" label="File" required class="required" />
+      </v-col>
+    </v-row>
+    <v-row v-if="!readonly">
+      <v-col cols="12">
+        <v-spacer />
+        <v-btn color="primary" small @click="add()">
+          Upload
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-list
+          two-line
+          subheader
+        >
+          <v-subheader v-if="value.length" inset>
+            Attachments
+          </v-subheader>
+
+          <AttachmentListItem
+            v-for="(item, index) in value"
+            :key="'attachment-' + index"
+            v-model="value[index]"
+          >
+            <template
+              v-if="!readonly"
+              v-slot:action
+            >
+              <v-btn
+                icon
+                color="error"
+                data-role="delete-attachment"
+                @click="remove(index)"
+              >
+                <v-icon>
+                  mdi-delete
+                </v-icon>
+              </v-btn>
+            </template>
+          </AttachmentListItem>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+/**
+ * @file provides a component for an attachment
+ * @author <marc.hanisch@gfz-potsdam.de>
+ */
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import { Attachment } from '../models/Attachment'
+
+// @ts-ignore
+import AttachmentListItem from './AttachmentListItem.vue'
+
+/**
+ * A class component for a list of Attachments and an upload form
+ * @extends Vue
+ */
+@Component({
+  components: { AttachmentListItem }
+})
+// @ts-ignore
+export default class AttachmentList extends Vue {
+  @Prop({
+    default: () => [] as Attachment[],
+    required: true,
+    type: Array
+  })
+  // @ts-ignore
+  value!: Attachment[]
+
+  @Prop({
+    default: false,
+    type: Boolean
+  })
+  // @ts-ignore
+  readonly: boolean
+
+  private attachment: Attachment = new Attachment()
+
+  /**
+   * adds a new Attachment instance
+   *
+   * @fires AttachmentList#input
+   */
+  add () {
+    /**
+     * Update event
+     * @event AttachmentList#input
+     * @type Attachment[]
+     */
+    this.$emit('input', [
+      ...this.value,
+      this.attachment
+    ] as Attachment[])
+
+    this.attachment = new Attachment()
+  }
+
+  /**
+   * removes as Attachment instance
+   *
+   * @param {number} index - the index of the property to remove
+   * @fires AttachmentList#input
+   */
+  remove (index: number) {
+    if (this.value[index]) {
+      const properties = [...this.value] as Attachment[]
+      properties.splice(index, 1)
+      /**
+       * Update event
+       * @event AttachmentList#input
+       * @type Attachment[]
+       */
+      this.$emit('input', properties)
+    }
+  }
+}
+</script>
