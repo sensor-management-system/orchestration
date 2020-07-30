82f46a6a365f0847871cd2d4351145ff4f4eb154
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2020-07-30 08:13:22 +0200

    Now the start_staging_gfz script uses docker-compose
    As I had trouble running the updated nuxt version in my
    old node version, I adopted the existing docker-compose file
    to be used with the addresses of the staging vms for gfz.
    
    I also needed to write an explicit docker file with the installation
    of python3 and the build-essential package.

--- start_staging_gfz.sh
+++ start_staging_gfz.sh
@@ -2,7 +2,7 @@
 
 # Make sure you are in the gfz network (locally or via vpn)
 # so that those servers are available
-export SMS_BACKEND_URL="http://rz-vm64.gfz-potsdam.de:5000"
-export CV_BACKEND_URL="http://rz-vm64.gfz-potsdam.de:5001"
-
-npm run dev
+# export SMS_BACKEND_URL="http://rz-vm64.gfz-potsdam.de:5000"
+# export CV_BACKEND_URL="http://rz-vm64.gfz-potsdam.de:5001"
+# --> those variables are now inside of the docker-compose file
+docker-compose -f docker-compose-gfz-local-with-staging-vm.yml up
