7507492e1f3f637282791ad4ae7215c428808d92
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2020-08-03 12:33:31 +0200

    Removed base_resource class (not working, can be reintroduced later)

--- app/project/api/resourceManager/device_detail.py
+++ app/project/api/resourceManager/device_detail.py
@@ -1,7 +1,6 @@
 from flask_rest_jsonapi import ResourceDetail
 from project.api.models.base_model import db
 from project.api.models.device import Device
-from project.api.resourceManager.base_resource import BaseResourceDetail
 from project.api.schemas.device_schema import DeviceSchema
 
 
@@ -11,12 +10,9 @@ class DeviceDetail(ResourceDetail):
      of an object, update an object and delete a Device
     """
 
-    def before_get_object(self, view_kwargs):
-        if view_kwargs.get('contact_id') is not None:
-            BaseResourceDetail.query_an_object(view_kwargs, 'contact')
 
     schema = DeviceSchema
     # decorators = (token_required,)
     data_layer = {'session': db.session,
                   'model': Device,
-                  'methods': {'before_get_object': before_get_object}}
+    }
