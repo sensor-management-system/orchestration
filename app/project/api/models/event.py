9e3a8f57f30948be5b5adcdcc9c98ba5d8cd64da
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2020-08-03 12:34:52 +0200

    Added default value for events

--- app/project/api/models/event.py
+++ app/project/api/models/event.py
@@ -1,3 +1,5 @@
+from sqlalchemy.sql import func
+
 from project.api.models.base_model import db
 
 
@@ -7,7 +9,7 @@ class Event(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     description = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    timestamp = db.Column(db.DateTime, default=func.now())
     device_id = db.Column(db.Integer, db.ForeignKey('device.id'),
                           nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
