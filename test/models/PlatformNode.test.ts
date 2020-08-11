12597e8aba12644a54b5439d8f30a22300a57baf
Marc Hanisch <mhanisch@gfz-potsdam.de>
2020-08-11 10:48:07 +0200

    tests children getter and setter

--- test/models/PlatformNode.test.ts
+++ test/models/PlatformNode.test.ts
@@ -24,10 +24,36 @@ describe('PlatformNode', () => {
     expect(secondNode.getTree()).not.toBe(firstNode.getTree())
   })
 
-  it('should have children', () => {
+  it('should be allowed to have children', () => {
     const platform = new Platform()
 
     const node = new PlatformNode(platform)
     expect(node.canHaveChildren()).toBeTruthy()
+  })
+
+  it('should return an Array for children', () => {
+    const firstPlatform = new Platform()
+    const firstNode = new PlatformNode(firstPlatform)
+
+    const secondPlatform = new Platform()
+    const secondNode = new PlatformNode(secondPlatform)
+
+    firstNode.getTree().push(secondNode)
+
+    expect(firstNode.children).toBeInstanceOf(Array)
+    expect(firstNode.children).toHaveLength(1)
+    expect(firstNode.children[0]).toBe(secondNode)
+  })
+
+  it('should set the tree from an array of children', () => {
+    const firstPlatform = new Platform()
+    const firstNode = new PlatformNode(firstPlatform)
+
+    const secondPlatform = new Platform()
+    const secondNode = new PlatformNode(secondPlatform)
+
+    firstNode.children = [secondNode]
+    expect(firstNode.getTree()).toHaveLength(1)
+    expect(firstNode.children[0]).toBe(secondNode)
   })
 })
