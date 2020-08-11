684da3a8d33a6d1f1230bdbcc88da7afcc44749b
Marc Hanisch <mhanisch@gfz-potsdam.de>
2020-08-11 10:09:19 +0200

    tests the add* and remove* methods

--- test/models/DeviceConfigurationAttributes.test.ts
+++ test/models/DeviceConfigurationAttributes.test.ts
@@ -31,17 +31,42 @@ describe('DeviceConfigurationAttributes', () => {
     expect(attributes).toHaveProperty('calibrationDate', now)
   })
 
-  it('should throw an error when assigning a property that is not a member of the device', () => {
+  it('should assign a deviceProperty that is a member of the device', () => {
     const prop1 = new DeviceProperty()
-    const prop2 = new DeviceProperty()
-    const prop3 = new DeviceProperty()
-
     const device = new Device()
-    device.id = 1
-    device.properties = [prop1, prop2]
+    device.properties = [prop1]
 
     const attributes = new DeviceConfigurationAttributes(device)
-    expect(() => { attributes.deviceProperties = [prop1, prop2, prop3]; return true }).toThrow(Error)
+    attributes.addDeviceProperty(prop1)
+    expect(attributes.deviceProperties).toHaveLength(1)
+  })
+
+  it('should throw an error when assigning a property that is not a member of the device', () => {
+    const prop1 = new DeviceProperty()
+    const device = new Device()
+
+    const attributes = new DeviceConfigurationAttributes(device)
+    expect(() => { attributes.addDeviceProperty(prop1) }).toThrow(Error)
+  })
+
+  it('should assign a deviceProperty that is a member of the device by its id', () => {
+    const prop1 = new DeviceProperty()
+    prop1.id = 1
+    const device = new Device()
+    device.properties = [prop1]
+
+    const attributes = new DeviceConfigurationAttributes(device)
+    attributes.addDevicePropertyById(prop1.id)
+    expect(attributes.deviceProperties).toHaveLength(1)
+  })
+
+  it('should throw an error when assigning a property by its id that is not a member of the device', () => {
+    const prop1 = new DeviceProperty()
+    prop1.id = 1
+    const device = new Device()
+
+    const attributes = new DeviceConfigurationAttributes(device)
+    expect(() => { attributes.addDevicePropertyById(prop1.id) }).toThrow(Error)
   })
 
   it('should set a property by its path', () => {
@@ -60,6 +85,40 @@ describe('DeviceConfigurationAttributes', () => {
     expect(attributes).toHaveProperty('offsetY', 1)
     expect(attributes).toHaveProperty('offsetZ', 1)
     expect(attributes).toHaveProperty('calibrationDate', now)
+  })
+
+  it('should remove a deviceProperty', () => {
+    const prop1 = new DeviceProperty()
+    const device = new Device()
+    device.properties = [prop1]
+
+    const attributes = new DeviceConfigurationAttributes(device)
+    attributes.addDeviceProperty(prop1)
+
+    attributes.removeDeviceProperty(prop1)
+    expect(attributes.deviceProperties).toHaveLength(0)
+  })
+
+  it('should remove a deviceProperty by its id', () => {
+    const prop1 = new DeviceProperty()
+    prop1.id = 1
+    const device = new Device()
+    device.properties = [prop1]
+
+    const attributes = new DeviceConfigurationAttributes(device)
+    attributes.addDeviceProperty(prop1)
+
+    attributes.removeDevicePropertyById(prop1.id)
+    expect(attributes.deviceProperties).toHaveLength(0)
+  })
+
+  it('should throw an error when removing a property that is not assigned', () => {
+    const prop1 = new DeviceProperty()
+    prop1.id = 1
+    const device = new Device()
+
+    const attributes = new DeviceConfigurationAttributes(device)
+    expect(() => { attributes.addDevicePropertyById(prop1.id) }).toThrow(Error)
   })
 
   it('should throw an error when using a invalid path', () => {
