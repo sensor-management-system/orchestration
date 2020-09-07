0790016c1053be8c7e624c0e4208d6f630a716ad
Marc Hanisch <mhanisch@gfz-potsdam.de>
2020-09-07 14:31:48 +0200

    adds parseIntOrDefault function

--- utils/numericsHelper.ts
+++ utils/numericsHelper.ts
@@ -1,1 +1,2 @@
 export const parseFloatOrDefault = (value: any, defaultValue: number | null = null) => isNaN(parseFloat(value)) ? defaultValue : parseFloat(value)
+export const parseIntOrDefault = (value: any, defaultValue: number | null = null) => isNaN(parseInt(value)) ? defaultValue : parseInt(value)
