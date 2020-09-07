a8be664fc5529acadf642985816f0c819ffc6e18
Marc Hanisch <mhanisch@gfz-potsdam.de>
2020-09-07 16:12:10 +0200

    adds a little Info component

---
+++ components/Info.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-alert
+    border="left"
+    colored-border
+    type="info"
+    elevation="2"
+    dismissible
+  >
+    <slot></slot>
+  </v-alert>
+</template>
+
+<script lang="ts">
+/**
+ * @file provides a component to show an info text
+ * @author <marc.hanisch@gfz-potsdam.de>
+ */
+import { Vue, Component } from 'nuxt-property-decorator'
+
+/**
+ * A class component to show an info text
+ * @extends Vue
+ */
+@Component
+// @ts-ignore
+export default class Info extends Vue {
+}
+</script>
