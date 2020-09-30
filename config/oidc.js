e92a87c84fc46ad92c031b12d46395ffb2c8eee1
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2020-09-30 15:47:21 +0200

    Set loadUserInfo to false to prevent problems on login

--- config/oidc.js
+++ config/oidc.js
@@ -11,5 +11,7 @@ export default {
   filterProtocolClaims: process.env.NUXT_ENV_FILTER_PROTOCOL_CLAIMS === 'true',
   automaticSilentRenew: process.env.NUXT_ENV_AUTOMATIC_SILENT_RENEW === 'false',
   silent_redirect_uri: process.env.NUXT_ENV_SILENT_REDIRECT_URI,
-  popupWindowFeatures: 'location=no,toolbar=no,width=500,height=600,left=100,top=100' // adjusted height
+  popupWindowFeatures: 'location=no,toolbar=no,width=500,height=600,left=100,top=100', // adjusted height
+  // TODO: Check if we need to put it into the CI, or if it ok for UFZ as well
+  loadUserInfo: false
 }
