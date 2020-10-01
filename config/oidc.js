bc517728d9cb44b3edafd923983cccfb20ff78cf
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2020-10-01 07:57:04 +0200

    Migrate loadUserInfo setting to dockerfile

--- config/oidc.js
+++ config/oidc.js
@@ -12,6 +12,5 @@ export default {
   automaticSilentRenew: process.env.NUXT_ENV_AUTOMATIC_SILENT_RENEW === 'false',
   silent_redirect_uri: process.env.NUXT_ENV_SILENT_REDIRECT_URI,
   popupWindowFeatures: 'location=no,toolbar=no,width=500,height=600,left=100,top=100', // adjusted height
-  // TODO: Check if we need to put it into the CI, or if it ok for UFZ as well
-  loadUserInfo: false
+  loadUserInfo: process.env.NUXT_ENV_LOAD_USER_INFO === 'true'
 }
