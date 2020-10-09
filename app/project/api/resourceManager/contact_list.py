d1664fd915ee1ede5f388fd9641ab90498d44b01
Martin Abbrent <martin.abbrent@ufz.de>
2020-10-09 15:39:40 +0200

    Filter device and platform in contacts related list view

--- app/project/api/resourceManager/contact_list.py
+++ app/project/api/resourceManager/contact_list.py
@@ -11,6 +11,52 @@ class ContactList(ResourceList):
     provides get and post methods to retrieve
      a collection of Contacts or create one.
     """
+
+    def query(self, view_kwargs):
+
+        query_ = self.session.query(Contact)
+        configuration_id = view_kwargs.get('configuration_id')
+        platform_id = view_kwargs.get('platform_id')
+        device_id = view_kwargs.get('device_id')
+
+        if configuration_id is not None:
+            try:
+                self.session.query(Configuration).filter_by(id=configuration_id).one()
+            except NoResultFound:
+                raise ObjectNotFound(
+                    {'parameter': 'id'},
+                    "Configuration: {} not found".format(configuration_id)
+                )
+            else:
+                query_ = query_.join(Contact.configurations).filter(
+                    Configuration.id == configuration_id)
+
+        if platform_id is not None:
+            try:
+                self.session.query(Platform).filter_by(id=platform_id).one()
+            except NoResultFound:
+                raise ObjectNotFound(
+                    {'parameter': 'id'},
+                    "Platform: {} not found".format(platform_id)
+                )
+            else:
+                query_ = query_.join(Contact.platforms).filter(
+                    Platform.id == platform_id)
+
+        if device_id is not None:
+            try:
+                self.session.query(Device).filter_by(id=device_id).one()
+            except NoResultFound:
+                raise ObjectNotFound(
+                    {'parameter': 'id'},
+                    "Device: {} not found".format(platform_id)
+                )
+            else:
+                query_ = query_.join(Contact.devices).filter(
+                    Device.id == device_id)
+
+        return query_
+
     schema = ContactSchema
     # decorators = (token_required,)
     data_layer = {'session': db.session,
