b0e130ef5a85febe6201d6d8fd12120bfdd2a282
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-10-19 15:12:35 +0200

    update relationships

--- app/project/api/models/configuration_platform.py
+++ app/project/api/models/configuration_platform.py
@@ -5,7 +5,6 @@ from project.api.models.platform import Platform
 
 
 class ConfigurationPlatform(db.Model, AuditMixin):
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     offset_x = db.Column(db.Float(), nullable=True)
     offset_y = db.Column(db.Float(), nullable=True)
@@ -15,15 +14,21 @@ class ConfigurationPlatform(db.Model, AuditMixin):
         db.Integer, db.ForeignKey("configuration.id"), nullable=False
     )
     configuration = db.relationship(
-        Configuration, uselist=False, foreign_keys=[configuration_id]
+        "Configuration", backref=db.backref("configuration_platform"), cascade="all"
     )
-
+    platform_id = db.Column(db.Integer, db.ForeignKey("platform.id"), nullable=False)
+    platform = db.relationship(
+        "Platform",
+        backref=db.backref("configuration_platform"),
+        foreign_keys=[platform_id],
+        cascade="all",
+    )
     parent_platform_id = db.Column(
         db.Integer, db.ForeignKey("platform.id"), nullable=True
     )
     parent_platform = db.relationship(
-        Platform, uselist=False, foreign_keys=[parent_platform_id]
+        "Platform",
+        backref=db.backref("inner_configuration_platform"),
+        foreign_keys=[parent_platform_id],
+        cascade="all",
     )
-
-    platform_id = db.Column(db.Integer, db.ForeignKey("platform.id"), nullable=False)
-    platform = db.relationship(Platform, uselist=False, foreign_keys=[platform_id])
