4334c17d7d56fea12582b2d090fd685e1e8f264d
Tobias Kuhnert <tobias.kuhnert@ufz.de>
2020-10-28 06:40:07 +0100

    added some updates

--- middleware/vuex-oidc-router.js
+++ middleware/vuex-oidc-router.js
@@ -2,7 +2,6 @@ export default function (context) {
   return new Promise((resolve, reject) => {
     context.store.dispatch('oidc/oidcCheckAccess', context.route)
            .then((hasAccess) => {
-             console.log('hasAccess',hasAccess);
              if (hasAccess) {
                resolve()
              }
