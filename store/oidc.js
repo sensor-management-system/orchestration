1177265afea616b2fb0fdbfcc9c6a0de3d1f5f55
alhajtah <kotyba.alhaj-taha@ufz.de>
2020-10-28 08:54:19 +0100

    fix env name

--- store/oidc.js
+++ store/oidc.js
@@ -2,9 +2,9 @@ import oidcSettings from "~/config/oidc";
 import {createAuthModul} from "~/utils/auth";
 
 
-const storeModule = createAuthModul(oidcSettings,{
+const storeModule = createAuthModul(oidcSettings, {
   namespaced: true,
-  routeBase: process.env.VUE_APP_PUBLIC_PATH
+  routeBase: process.env.NUXT_ENV_PUBLIC_PATH
 })
 
 export const state = () => (storeModule.state)
