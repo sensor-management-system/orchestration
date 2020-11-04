3226f4539ef33d5ec1491ce600dcda711f6c7484
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2020-11-04 08:01:09 +0100

    I added some more test cases for the calibration date values.
    * Some more test cases for the handling of the calibration date values
      in the configuration hierarchy. (Just to make sure that datetime objects
      work properly as well).
    * I also reformatted the file with black.
    * I also checked the data result for the basic get request.

--- app/project/tests/test_configurations.py
+++ app/project/tests/test_configurations.py
@@ -15,18 +15,23 @@ from project.tests.read_from_json import extract_data_from_json_file
 
 class TestConfigurationsService(BaseTestCase):
     """Tests for the Configurations Service."""
-    configurations_url = base_url + '/configurations'
-    platform_url = base_url + '/platforms'
-    device_url = base_url + '/devices'
-    object_type = 'configuration'
+
+    configurations_url = base_url + "/configurations"
+    platform_url = base_url + "/platforms"
+    device_url = base_url + "/devices"
+    object_type = "configuration"
     json_data_url = "/usr/src/app/project/tests/drafts/configurations_test_data.json"
     device_json_data_url = "/usr/src/app/project/tests/drafts/devices_test_data.json"
-    platform_json_data_url = "/usr/src/app/project/tests/drafts/platforms_test_data.json"
+    platform_json_data_url = (
+        "/usr/src/app/project/tests/drafts/platforms_test_data.json"
+    )
 
     def test_get_configurations(self):
         """Ensure the GET /configurations route behaves correctly."""
         response = self.client.get(self.configurations_url)
         self.assertEqual(response.status_code, 200)
+        # There are no data sets inserted yet.
+        self.assertEqual(response.json["data"], [])
 
     def test_add_configuration_model(self):
         """""Ensure Add Configuration model """
@@ -52,8 +57,11 @@ class TestConfigurationsService(BaseTestCase):
         db.session.commit()
 
         platform1_conf = ConfigurationPlatform(
-            platform=platform1, configuration=config1, offset_x=1.0,
-            offset_y=1.0, offset_z=1.0
+            platform=platform1,
+            configuration=config1,
+            offset_x=1.0,
+            offset_y=1.0,
+            offset_z=1.0,
         )
         db.session.add(platform1_conf)
 
@@ -61,27 +69,44 @@ class TestConfigurationsService(BaseTestCase):
             platform=platform2,
             configuration=config1,
             parent_platform=platform1,
-            offset_x=2.0, offset_y=2.0, offset_z=2.0
+            offset_x=2.0,
+            offset_y=2.0,
+            offset_z=2.0,
         )
         platform3_conf = ConfigurationPlatform(
-            platform=platform3, configuration=config1, offset_x=13.5,
-            offset_y=13.5, offset_z=13.5
+            platform=platform3,
+            configuration=config1,
+            offset_x=13.5,
+            offset_y=13.5,
+            offset_z=13.5,
         )
 
         db.session.add(platform2_conf)
         db.session.add(platform3_conf)
 
         device1_conf = ConfigurationDevice(
-            device=device1, configuration=config1, parent_platform=platform2, offset_x=0.5,
-            offset_y=0.5, offset_z=0.5
+            device=device1,
+            configuration=config1,
+            parent_platform=platform2,
+            offset_x=0.5,
+            offset_y=0.5,
+            offset_z=0.5,
         )
         device2_conf = ConfigurationDevice(
-            device=device2, configuration=config1, parent_platform=platform2, offset_x=0.6,
-            offset_y=0.6, offset_z=0.6
+            device=device2,
+            configuration=config1,
+            parent_platform=platform2,
+            offset_x=0.6,
+            offset_y=0.6,
+            offset_z=0.6,
         )
         device3_conf = ConfigurationDevice(
-            device=device3, configuration=config1, parent_platform=platform2, offset_x=0.65,
-            offset_y=0.65, offset_z=0.65
+            device=device3,
+            configuration=config1,
+            parent_platform=platform2,
+            offset_x=0.65,
+            offset_y=0.65,
+            offset_z=0.65,
         )
 
         db.session.add(device1_conf)
@@ -91,106 +116,133 @@ class TestConfigurationsService(BaseTestCase):
         db.session.commit()
 
         c = db.session.query(Configuration).filter_by(label="Config1").first()
-
         self.assertEqual("static", c.location_type)
 
     def test_add_configuration(self):
         """Ensure POST a new configuration can be added to the database."""
-        devices_json = extract_data_from_json_file(
-            self.device_json_data_url,
-            "devices")
 
-        device_data = {
-            "data": {
-                "type": "device",
-                "attributes": devices_json[0]}
-
+        # we want to run the very same test with multiple dates
+        calibration_dates = {
+            "20201111": {
+                "json_api_value": "2020-11-11T00:00:00",
+                "sql_alchemy_value": datetime.datetime(
+                    year=2020, month=11, day=11, hour=0, minute=0, second=0
+                ),
+            },
+            "20200229": {
+                "json_api_value": "2020-02-29T00:00:00",
+                "sql_alchemy_value": datetime.datetime(
+                    year=2020, month=2, day=29, hour=0, minute=0, second=0
+                ),
+            },
+            "2020-08-29T13:49:48.015620+00:00": {
+                "json_api_value": "2020-08-29T13:49:48.015620",
+                # first this should be wrong
+                "sql_alchemy_value": datetime.datetime(
+                    year=2020,
+                    month=8,
+                    day=29,
+                    hour=13,
+                    minute=49,
+                    second=48,
+                    microsecond=15620,
+                ),
+            },
         }
-        super().add_object(
-            url=self.device_url, data_object=device_data,
-            object_type='device')
+        for (
+            input_calibration_date,
+            expected_output_calibration_date,
+        ) in calibration_dates.items():
+            # set up for each single run
+            self.setUp()
 
-        platforms_json = extract_data_from_json_file(
-            self.platform_json_data_url,
-            "platforms")
+            devices_json = extract_data_from_json_file(
+                self.device_json_data_url, "devices"
+            )
 
-        platform_data = {
-            "data": {
-                "type": "platform",
-                "attributes": platforms_json[0]
+            device_data = {"data": {"type": "device", "attributes": devices_json[0]}}
+            super().add_object(
+                url=self.device_url, data_object=device_data, object_type="device"
+            )
+
+            platforms_json = extract_data_from_json_file(
+                self.platform_json_data_url, "platforms"
+            )
+
+            platform_data = {
+                "data": {"type": "platform", "attributes": platforms_json[0]}
             }
-        }
 
-        super().add_object(
-            url=self.platform_url, data_object=platform_data,
-            object_type='platform')
+            super().add_object(
+                url=self.platform_url, data_object=platform_data, object_type="platform"
+            )
 
-        config_json = extract_data_from_json_file(
-            self.json_data_url,
-            "configuration")
+            config_json = extract_data_from_json_file(
+                self.json_data_url, "configuration"
+            )
 
-        config_data = {
-            "data": {
-                "type": "configuration",
-                "attributes": config_json[0]}
+            config_json[0]["hierarchy"][0]["children"][0][
+                "calibration_date"
+            ] = input_calibration_date
 
-        }
-        res = super().add_object(
-            url=self.configurations_url, data_object=config_data,
-            object_type=self.object_type)
-        self.assertEqual('2020-11-11T00:00:00',
-                         res['data']['attributes']['hierarchy'][0]['children'][0]
-                         ['calibration_date'])
+            config_data = {
+                "data": {"type": "configuration", "attributes": config_json[0]}
+            }
+            res = super().add_object(
+                url=self.configurations_url,
+                data_object=config_data,
+                object_type=self.object_type,
+            )
+            self.assertEqual(
+                expected_output_calibration_date["json_api_value"],
+                res["data"]["attributes"]["hierarchy"][0]["children"][0][
+                    "calibration_date"
+                ],
+            )
+
+            configuration_device = (
+                db.session.query(ConfigurationDevice)
+                .filter_by(device_id=1, configuration_id=1)
+                .first()
+            )
+            self.assertEqual(
+                configuration_device.calibration_date,
+                expected_output_calibration_date["sql_alchemy_value"],
+            )
+
+            # clean up after each run
+            self.tearDown()
 
     def test_add_configuration_with_int_as_calibration_type(self):
         """Ensure That a Post for a new configuration
         with value-type for calibration-date = integer throw
         a type error."""
 
-        devices_json = extract_data_from_json_file(
-            self.device_json_data_url,
-            "devices")
+        devices_json = extract_data_from_json_file(self.device_json_data_url, "devices")
 
-        device_data = {
-            "data": {
-                "type": "device",
-                "attributes": devices_json[0]}
-
-        }
+        device_data = {"data": {"type": "device", "attributes": devices_json[0]}}
         super().add_object(
-            url=self.device_url, data_object=device_data,
-            object_type='device')
+            url=self.device_url, data_object=device_data, object_type="device"
+        )
 
         platforms_json = extract_data_from_json_file(
-            self.platform_json_data_url,
-            "platforms")
+            self.platform_json_data_url, "platforms"
+        )
 
-        platform_data = {
-            "data": {
-                "type": "platform",
-                "attributes": platforms_json[0]
-            }
-        }
+        platform_data = {"data": {"type": "platform", "attributes": platforms_json[0]}}
 
         super().add_object(
-            url=self.platform_url, data_object=platform_data,
-            object_type='platform')
+            url=self.platform_url, data_object=platform_data, object_type="platform"
+        )
 
-        config_json = extract_data_from_json_file(
-            self.json_data_url,
-            "configuration")
+        config_json = extract_data_from_json_file(self.json_data_url, "configuration")
 
-        config_data = {
-            "data": {
-                "type": "configuration",
-                "attributes": config_json[1]}
-
-        }
+        config_data = {"data": {"type": "configuration", "attributes": config_json[1]}}
         with self.client:
             response = self.client.post(
                 self.configurations_url,
                 data=json.dumps(config_data),
-                content_type='application/vnd.api+json',
+                content_type="application/vnd.api+json",
             )
         json.loads(response.data.decode())
         self.assertEqual(response.status_code, 500)
@@ -200,54 +252,35 @@ class TestConfigurationsService(BaseTestCase):
         with false string value for calibration-date throw
         a type error."""
 
-        devices_json = extract_data_from_json_file(
-            self.device_json_data_url,
-            "devices")
+        devices_json = extract_data_from_json_file(self.device_json_data_url, "devices")
 
-        device_data = {
-            "data": {
-                "type": "device",
-                "attributes": devices_json[0]}
-
-        }
+        device_data = {"data": {"type": "device", "attributes": devices_json[0]}}
         super().add_object(
-            url=self.device_url, data_object=device_data,
-            object_type='device')
+            url=self.device_url, data_object=device_data, object_type="device"
+        )
 
         platforms_json = extract_data_from_json_file(
-            self.platform_json_data_url,
-            "platforms")
+            self.platform_json_data_url, "platforms"
+        )
 
-        platform_data = {
-            "data": {
-                "type": "platform",
-                "attributes": platforms_json[0]
-            }
-        }
+        platform_data = {"data": {"type": "platform", "attributes": platforms_json[0]}}
 
         super().add_object(
-            url=self.platform_url, data_object=platform_data,
-            object_type='platform')
+            url=self.platform_url, data_object=platform_data, object_type="platform"
+        )
 
-        config_json = extract_data_from_json_file(
-            self.json_data_url,
-            "configuration")
+        config_json = extract_data_from_json_file(self.json_data_url, "configuration")
 
-        config_data = {
-            "data": {
-                "type": "configuration",
-                "attributes": config_json[2]}
-
-        }
+        config_data = {"data": {"type": "configuration", "attributes": config_json[2]}}
         with self.client:
             response = self.client.post(
                 self.configurations_url,
                 data=json.dumps(config_data),
-                content_type='application/vnd.api+json',
+                content_type="application/vnd.api+json",
             )
         json.loads(response.data.decode())
         self.assertEqual(response.status_code, 500)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
