aff786db0e9f6b4e7a1672058887e43afc368ee9
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2020-11-23 08:39:30 +0100

    Very first way to query via elasticsearch

--- app/project/api/resourceManager/platform_list.py
+++ app/project/api/resourceManager/platform_list.py
@@ -4,6 +4,7 @@ from project.api.models.platform import Platform
 from project.api.schemas.platform_schema import PlatformSchema
 from project.api.token_checker import token_required
 
+from flask import request
 
 class PlatformList(ResourceList):
     """
@@ -11,8 +12,23 @@ class PlatformList(ResourceList):
     only POST and GET method allowed
     """
 
+    def query(self, view_kwargs):
+        """Extension to query platforms via elasticsearch."""
+        query = self.session.query(Platform)
+
+        rargs = request.args
+        if rargs.get('q') is not None:
+            page = rargs.get('page[number]', 1)
+            per_page = rargs.get('page[size]', 20)
+            query, _total = Platform.search(rargs['q'], page, per_page)
+        return query
+
+
     schema = PlatformSchema
     # decorators = (token_required,)
     data_layer = {'session': db.session,
-                  'model': Platform
+                  'model': Platform,
+                  'methods': {
+                      'query': query,
+                    }
                   }
