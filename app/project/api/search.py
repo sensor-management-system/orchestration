cf1ebee9c2107c72528f145813d74446e74df7af
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2020-11-23 11:13:37 +0100

    Fix flake8 issue (and black incompatibility)

--- app/project/api/search.py
+++ app/project/api/search.py
@@ -21,7 +21,12 @@ def query_index(index, query, page, per_page):
         return [], 0
     search = current_app.elasticsearch.search(
         index=index,
-        body={"query": query, "from": (page - 1) * per_page, "size": per_page,},
+        body={
+            "query": query,
+            # the from value is the beginning & starts counting with 0
+            "from": (page - 1) * per_page,
+            "size": per_page,
+        },
     )
     ids = [int(hit["_id"]) for hit in search["hits"]["hits"]]
     return ids, search["hits"]["total"]["value"]
