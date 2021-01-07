07d5bcbad34752ec9fa4dbb1cd489f92ac3cca48
alhajtah <kotyba.alhaj-taha@ufz.de>
2021-01-07 16:45:40 +0100

    use jwt

--- app/project/config.py
+++ app/project/config.py
@@ -1,4 +1,8 @@
+import json
 import os
+
+from jwt.algorithms import RSAAlgorithm
+from project.api.token_checker import oidc_jwks_uri
 
 
 class BaseConfig:
@@ -17,6 +21,16 @@ class DevelopmentConfig(BaseConfig):
 
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL")
+    # Setup Token Verification
+    # force use of RS265
+    JWT_ALGORITHM = "RS256"
+    # retrieve first jwk entry from jwks_uri endpoint and use it to construct the RSA public key
+    JWT_PUBLIC_KEY = RSAAlgorithm.from_jwk(json.dumps(oidc_jwks_uri["keys"][0]))
+    # audience is oidc client id (can be array starting
+    # https://github.com/vimalloc/flask-jwt-extended/issues/219)
+    JWT_DECODE_AUDIENCE = ["rdmsvm-implicit-flow", "oidcdebugger-implicit-flow"]
+    # name of token entry that will become distinct flask identity username
+    JWT_IDENTITY_CLAIM = os.environ.get("OIDC_USERNAME_CLAIM")
 
 
 class TestingConfig(BaseConfig):
