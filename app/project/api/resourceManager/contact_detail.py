db08d69dac3bc8c1b927ea65a5c7e7ea0eb1e262
alhajtah <kotyba.alhaj-taha@ufz.de>
2021-01-12 15:06:12 +0100

    id instead of contact_id

--- app/project/api/resourceManager/contact_detail.py
+++ app/project/api/resourceManager/contact_detail.py
@@ -19,7 +19,7 @@ class ContactDetail(ResourceDetail):
                 _ = self.session.query(Contact).filter_by(id=view_kwargs["id"]).one()
             except NoResultFound:
                 raise ObjectNotFound(
-                    {"parameter": "contact_id"},
+                    {"parameter": "id"},
                     "Contact: {} not found".format(view_kwargs["id"]),
                 )
 
