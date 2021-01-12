f1ff97951994c1b0a8b3b1478d5e6412743c6026
alhajtah <kotyba.alhaj-taha@ufz.de>
2021-01-12 12:38:02 +0100

    check if contact already exists

--- app/project/api/token_checker.py
+++ app/project/api/token_checker.py
@@ -4,7 +4,6 @@ from functools import wraps
 import requests
 from flask import Blueprint, request
 from flask_jwt_extended import JWTManager, get_raw_jwt, verify_jwt_in_request
-from flask_login import LoginManager
 from project.api.models.base_model import db
 from project.api.models.contact import Contact
 from project.api.models.user import User
@@ -19,8 +18,6 @@ oidc_config = requests.get(OIDC_ISSUER_URL, verify=False).json()
 # retrieve data from jwks_uri endpoint
 oidc_jwks_uri = requests.get(oidc_config["jwks_uri"], verify=False).json()
 
-login_manager = LoginManager()
-
 
 def token_required(fn):
     @wraps(fn)
@@ -34,20 +31,29 @@ def token_required(fn):
 
 @jwt.user_loader_callback_loader
 def add_user_to_database(current_user):
-    exists = db.session.query(User).filter_by(subject=current_user).first()
-    if not exists:
-        given_name = get_raw_jwt()["given_name"]
-        family_name = get_raw_jwt()["family_name"]
-        email = get_raw_jwt()["email"]
-        subject = get_raw_jwt()["sub"]
-        contact = Contact(given_name=given_name, family_name=family_name, email=email)
-        db.session.add(contact)
-        db.session.commit()
+    current_user_exists = db.session.query(User).filter_by(subject=current_user).first()
+    if not current_user_exists:
+        raw_jwt_object = get_raw_jwt()
+        given_name = raw_jwt_object["given_name"]
+        family_name = raw_jwt_object["family_name"]
+        email = raw_jwt_object["email"]
+        subject = raw_jwt_object["sub"]
+        current_contact_exists = db.session.query(Contact).filter_by(email=email).first()
+        contact = add_contact_if_not_exists(current_contact_exists, email, family_name, given_name)
         user = User(subject=subject, contact_id=contact.id)
         db.session.add(user)
         db.session.commit()
         return user
-    return exists
+    return current_user_exists
+
+
+def add_contact_if_not_exists(current_contact_exists, email, family_name, given_name):
+    if not current_contact_exists:
+        contact = Contact(given_name=given_name, family_name=family_name, email=email)
+        db.session.add(contact)
+        db.session.commit()
+        return contact
+    return current_contact_exists
 
 
 @auth_blueprint.route("/rdm/svm-api/v1/auth", methods=["GET"])
