0dd5c42346e9ee4e336ad660f1451f368b918fa8
alhajtah <kotyba.alhaj-taha@ufz.de>
2021-01-12 14:14:24 +0100

    Use longer variable names & add Note to docs

--- app/project/api/resourceManager/base_resource.py
+++ app/project/api/resourceManager/base_resource.py
@@ -6,21 +6,25 @@ from project.api.models.user import User
 
 def add_create_by_id(data):
     """
-    Use jwt to add user id to dataset
+    Use jwt to add user id to dataset.
     :param data:
     :param args:
     :param kwargs:
     :return:
+
+    .. note:: every HTTP-Methode should come with a json web token, which automatically
+    check if the user exists or add the user to the database
+    so that a user can't be None. Due to that created_by_id can't be None also.
     """
     current_user = get_jwt_identity()
-    u = db.session.query(User).filter_by(subject=current_user).first()
-    data["created_by_id"] = u.id
+    user_entry = db.session.query(User).filter_by(subject=current_user).first()
+    data["created_by_id"] = user_entry.id
 
 
-def add_contact_to_object(d):
-    u = db.session.query(User).filter_by(id=d.created_by_id).first()
-    contact_id = u.contact_id
-    c = db.session.query(Contact).filter_by(id=contact_id).first()
-    d.contacts.append(c)
-    db.session.add(d)
+def add_contact_to_object(entity_with_contact_list):
+    user_entry = db.session.query(User).filter_by(id=entity_with_contact_list.created_by_id).first()
+    contact_id = user_entry.contact_id
+    contact_entry = db.session.query(Contact).filter_by(id=contact_id).first()
+    entity_with_contact_list.contacts.append(contact_entry)
+    db.session.add(entity_with_contact_list)
     db.session.commit()
