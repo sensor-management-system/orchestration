d4338f9e9314cbc07bdabe727338db08d7f05b79
Marc Hanisch <mhanisch@gfz-potsdam.de>
2021-01-13 16:03:20 +0100

    adds model, api and serializer for MeasuredQuantityUnits

---
+++ services/cv/MeasuredQuantityUnitApi.ts
@@ -0,0 +1,93 @@
+/**
+ * @license
+ * Web client of the Sensor Management System software developed within
+ * the Helmholtz DataHub Initiative by GFZ and UFZ.
+ *
+ * Copyright (C) 2020
+ * - Nils Brinckmann (GFZ, nils.brinckmann@gfz-potsdam.de)
+ * - Marc Hanisch (GFZ, marc.hanisch@gfz-potsdam.de)
+ * - Helmholtz Centre Potsdam - GFZ German Research Centre for
+ *   Geosciences (GFZ, https://www.gfz-potsdam.de)
+ *
+ * Parts of this program were developed within the context of the
+ * following publicly funded projects or measures:
+ * - Helmholtz Earth and Environment DataHub
+ *   (https://www.helmholtz.de/en/research/earth_and_environment/initiatives/#h51095)
+ *
+ * Licensed under the HEESIL, Version 1.0 or - as soon they will be
+ * approved by the "Community" - subsequent versions of the HEESIL
+ * (the "Licence").
+ *
+ * You may not use this work except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * https://gitext.gfz-potsdam.de/software/heesil
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+import { AxiosInstance, AxiosResponse } from 'axios'
+
+import { MeasuredQuantityUnit } from '@/models/MeasuredQuantityUnit'
+import { MeasuredQuantityUnitSerializer } from '@/serializers/jsonapi/MeasuredQuantityUnitSerializer'
+
+export class MeasuredQuantityUnitApi {
+  private axiosApi: AxiosInstance
+  private serializer: MeasuredQuantityUnitSerializer
+
+  constructor (axiosInstance: AxiosInstance) {
+    this.axiosApi = axiosInstance
+    this.serializer = new MeasuredQuantityUnitSerializer()
+  }
+
+  newSearchBuilder (): MeasuredQuantityUnitSearchBuilder {
+    return new MeasuredQuantityUnitSearchBuilder(this.axiosApi, this.serializer)
+  }
+
+  findAll (): Promise<MeasuredQuantityUnit[]> {
+    return this.newSearchBuilder().build().findMatchingAsList()
+  }
+}
+
+export class MeasuredQuantityUnitSearchBuilder {
+  private axiosApi: AxiosInstance
+  private serializer: MeasuredQuantityUnitSerializer
+
+  constructor (axiosApi: AxiosInstance, serializer: MeasuredQuantityUnitSerializer) {
+    this.axiosApi = axiosApi
+    this.serializer = serializer
+  }
+
+  build (): MeasuredQuantityUnitSearcher {
+    return new MeasuredQuantityUnitSearcher(this.axiosApi, this.serializer)
+  }
+}
+
+export class MeasuredQuantityUnitSearcher {
+  private axiosApi: AxiosInstance
+  private serializer: MeasuredQuantityUnitSerializer
+
+  constructor (axiosApi: AxiosInstance, serializer: MeasuredQuantityUnitSerializer) {
+    this.axiosApi = axiosApi
+    this.serializer = serializer
+  }
+
+  findMatchingAsList (): Promise<MeasuredQuantityUnit[]> {
+    return this.axiosApi.get(
+      '',
+      {
+        params: {
+          'page[size]': 10000,
+          include: 'unit'
+        }
+      }
+    ).then((rawResponse) => {
+      const response = rawResponse.data
+      this.serializer.included = response.included
+      return this.serializer.convertJsonApiObjectListToModelList(response)
+    })
+  }
+}
