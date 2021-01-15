d8a3929c74b066da1aec49fbd6dbc4ae5d2a8dc1
Marc Hanisch <mhanisch@gfz-potsdam.de>
2021-01-15 15:03:07 +0100

    adds base class for paginated loading

--- services/cv/UnitApi.ts
+++ services/cv/UnitApi.ts
@@ -33,13 +33,15 @@ import { AxiosInstance } from 'axios'
 
 import { Unit } from '@/models/Unit'
 import { UnitSerializer } from '@/serializers/jsonapi/UnitSerializer'
+import { CVApi } from '@/services/cv/CVApi'
 
-export class UnitApi {
-  private axiosApi: AxiosInstance
+import { IPaginationLoader } from '@/utils/PaginatedLoader'
+
+export class UnitApi extends CVApi<Unit> {
   private serializer: UnitSerializer
 
   constructor (axiosInstance: AxiosInstance) {
-    this.axiosApi = axiosInstance
+    super(axiosInstance)
     this.serializer = new UnitSerializer()
   }
 
@@ -49,6 +51,10 @@ export class UnitApi {
 
   findAll (): Promise<Unit[]> {
     return this.newSearchBuilder().build().findMatchingAsList()
+  }
+
+  findAllPaginated (pageSize: number = 100): Promise<Unit[]> {
+    return this.newSearchBuilder().build().findMatchingAsPaginationLoader(pageSize).then(loader => this.loadPaginated(loader))
   }
 }
 
@@ -75,6 +81,35 @@ export class UnitSearcher {
     this.serializer = serializer
   }
 
+  private findAllOnPage (page: number, pageSize: number): Promise<IPaginationLoader<Unit>> {
+    return this.axiosApi.get(
+      '',
+      {
+        params: {
+          'page[size]': pageSize,
+          'page[number]': page,
+          'filter[status.iexact]': 'ACCEPTED',
+          sort: 'term'
+        }
+      }
+    ).then((rawResponse) => {
+      const response = rawResponse.data
+      const elements: Unit[] = this.serializer.convertJsonApiObjectListToModelList(response)
+      const totalCount = response.meta.pagination.count
+
+      let funToLoadNext = null
+      if (response.meta.pagination.page < response.meta.pagination.pages) {
+        funToLoadNext = () => this.findAllOnPage(page + 1, pageSize)
+      }
+
+      return {
+        elements,
+        totalCount,
+        funToLoadNext
+      }
+    })
+  }
+
   findMatchingAsList (): Promise<Unit[]> {
     return this.axiosApi.get(
       '',
@@ -89,5 +124,9 @@ export class UnitSearcher {
       const response = rawResponse.data
       return this.serializer.convertJsonApiObjectListToModelList(response)
     })
+  }
+
+  findMatchingAsPaginationLoader (pageSize: number): Promise<IPaginationLoader<Unit>> {
+    return this.findAllOnPage(1, pageSize)
   }
 }
