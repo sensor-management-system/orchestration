46da0b80f65c363394b6aa31cd3fb99867b9a445
alhajtah <kotyba.alhaj-taha@ufz.de>
2021-02-02 11:48:13 +0100

    Make mock jwt similar to IDP: - Use flask_jwt_extended to mock a jwt - Remove in code run test with jwt dependencies

--- app/project/tests/base.py
+++ app/project/tests/base.py
@@ -1,30 +1,99 @@
 import json
+import time
 
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended.tokens import _encode_jwt
 from flask_testing import TestCase
 
 from project import create_app
 from project.api.models.base_model import db
-from project.api.token_checker import jwt
 
 app = create_app()
 
 
-@jwt.user_claims_loader
-def add_claims_to_access_token(identity):
-    return {
-        "sub": identity,
-        "email": "test-user@test.de",
-        "name": "test user",
-        "family_name": "user",
-        "given_name": "test",
-    }
+def encode_token_date_with_hs256(
+    token_data,
+    headers=None,
+):
+    """
+    Make use of the flask_jwt_extended methode (_encode_jwt) to
+    encode our payload.
+    The test uses "HS256" as encode algorithm.
+
+    :param token_data:
+    :param identity: Identifier for who this token is for (ex, sub).
+    :param fresh: this will indicate how long this
+                  token will remain fresh.
+    :param expires_delta: How far in the future this token should expire
+    :param headers:
+    :return:
+    """
+    return _encode_jwt(
+        token_data,
+        expires_delta=None,
+        secret=app.config["JWT_SECRET_KEY"],
+        algorithm="HS256",
+        json_encoder=None,
+        headers=headers,
+    )
 
 
 def create_token():
-    hs256_token = create_access_token("testusr@test.de")
+    """
+    Mock a 'HS256' jwt same to the one, that come from idp
+     and prepare the access header.
+
+    :return: an access token dict for the request headers
+    """
+    token_data = generate_token_data()
+    hs256_token = encode_token_date_with_hs256(token_data)
     access_headers = {"Authorization": "Bearer {}".format(hs256_token)}
     return access_headers
+
+
+def generate_token_data():
+    """
+    Generate jwt payload data.
+
+    :return: identity & payload
+
+        :Example:
+            {
+              "iat": 1612259403,
+              "sub": "jasonmcp@unittest.test",
+              "iss": "SMS unittest",
+              "name": "Jason Mcpherson",
+              "family_name": "mcpherson",
+              "given_name": "jason",
+              "email": "jason.mcpherson@unittest.test",
+              "exp": 1612259463,
+              "aud": "SMS"
+            }
+    """
+    # get current time in seconds
+    now = int(time.time())
+    from faker import Faker
+
+    f = Faker()
+    # generate a random name will bw like "test test"
+    name = f.unique.name()
+    # separate the name to a list
+    name_l = name.lower().split(" ")
+    family_name = name_l[1]
+    given_name = name_l[0]
+    # Should be like the sub which comes from the IDP
+    identity = f"{''.join(name_l)[:8]}@unittest.test"
+    token_data = {
+        "sub": identity,
+        "iss": "SMS unittest",
+        "name": name,
+        "family_name": family_name,
+        "given_name": given_name,
+        "email": f"{given_name}.{family_name}@unittest.test",
+        "iat": now,  # Issued At: Date/time when the token was issued
+        "exp": now + 60,  # Expiration: expire after one minute.
+        "aud": "SMS",  # recipient of this token
+    }
+    return token_data
 
 
 class BaseTestCase(TestCase):
