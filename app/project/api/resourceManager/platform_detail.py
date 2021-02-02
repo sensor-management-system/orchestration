93a2057ec39f64f92646f4cfd321c1e84919a883
Kotyba Alhaj Taha <kotyba.alhaj-taha@ufz.de>
2021-02-02 14:44:23 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- app/project/api/resourceManager/platform_detail.py
+++ app/project/api/resourceManager/platform_detail.py
@@ -28,7 +28,7 @@ def delete_attachments_in_minio_by_device_id(platform_id_intended_for_deletion):
 class PlatformDetail(ResourceDetail):
     """
     provides get, patch and delete methods to retrieve details
-    of an object, update an object and delete an Event
+    of an object, update an object and delete an Event.
     """
 
     def before_get_object(self, view_kwargs):
