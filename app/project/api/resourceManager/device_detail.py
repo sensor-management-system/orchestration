5a8b10cdcf3f9d066898c7f1d008aa5f013abe45
Kotyba Alhaj Taha <kotyba.alhaj-taha@ufz.de>
2021-02-02 14:45:37 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- app/project/api/resourceManager/device_detail.py
+++ app/project/api/resourceManager/device_detail.py
@@ -27,7 +27,7 @@ def delete_attachments_in_minio_by_device_id(device_id_intended_for_deletion):
 
 class DeviceDetail(ResourceDetail):
     """
-    provides get, patch and delete methods to retrieve details
+    Provides get, patch and delete methods to retrieve details
     of an object, update an object and delete a device.
     """
 
