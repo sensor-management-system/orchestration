9d74f9a24c94760f19a093b27c0fd5310f89d90a
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2021-03-01 09:36:28 +0100

    Again switched to plural for backrefs (links to actions)

--- app/project/api/models/unmount_actions.py
+++ app/project/api/models/unmount_actions.py
@@ -9,16 +9,16 @@ class PlatformUnmountAction(db.Model, AuditMixin):
     )
     configuration = db.relationship(
         "Configuration", uselist=False, foreign_keys=[configuration_id],
-        backref=db.backref("platform_unmount_action")
+        backref=db.backref("platform_unmount_actions")
     )
     platform_id = db.Column(db.Integer, db.ForeignKey("platform.id"), nullable=False)
     platform = db.relationship("Platform", uselist=False, foreign_keys=[platform_id],
-                               backref=db.backref("platform_unmount_action"))
+                               backref=db.backref("platform_unmount_actions"))
     end_date = db.Column(db.DateTime, nullable=False)
     description = db.Column(db.Text, nullable=True)
     contact_id = db.Column(db.Integer, db.ForeignKey("contact.id"), nullable=False)
     contact = db.relationship("Contact", uselist=False, foreign_keys=[contact_id],
-                              backref=db.backref("platform_unmount_action"))
+                              backref=db.backref("platform_unmount_actions"))
 
 
 class DeviceUnmountAction(db.Model, AuditMixin):
@@ -32,9 +32,9 @@ class DeviceUnmountAction(db.Model, AuditMixin):
     )
     device_id = db.Column(db.Integer, db.ForeignKey("device.id"), nullable=False)
     device = db.relationship("Device", uselist=False, foreign_keys=[device_id],
-                             backref=db.backref("device_unmount_action"))
+                             backref=db.backref("device_unmount_actions"))
     end_date = db.Column(db.DateTime, nullable=False)
     description = db.Column(db.Text, nullable=True)
     contact_id = db.Column(db.Integer, db.ForeignKey("contact.id"), nullable=False)
     contact = db.relationship("Contact", uselist=False, foreign_keys=[contact_id],
-                              backref=db.backref("device_unmount_action"))
+                              backref=db.backref("device_unmount_actions"))
