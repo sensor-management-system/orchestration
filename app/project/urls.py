420c6785b6167ad56edd6f1ad5b783528d383fa0
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2021-03-04 08:59:50 +0100

    Fix typo

--- app/project/urls.py
+++ app/project/urls.py
@@ -167,7 +167,7 @@ api.route(
 )
 api.route(
     DeviceAttachmentRelationship,
-    "device_attachemnt_device",
+    "device_attachment_device",
     "/device-attachments/<int:id>/relationships/device",
 )
 
