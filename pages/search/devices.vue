0121a8f48ab84cc867beb10b303e01fbd7b330bc
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2021-03-24 14:41:09 +0100

    Added confirming dialog for custom fields
    And I tried to make the delete entry more consistent

--- pages/search/devices.vue
+++ pages/search/devices.vue
@@ -255,7 +255,7 @@ permissions and limitations under the Licence.
                           >
                             mdi-delete
                           </v-icon>
-                          Delete
+                          Delete...
                         </v-list-item-title>
                       </v-list-item-content>
                     </v-list-item>
