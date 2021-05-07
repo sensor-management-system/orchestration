c7b215ee9de37c66973935a2d45bd021532a17b0
Marc Hanisch <mhanisch@gfz-potsdam.de>
2021-05-07 12:52:20 +0200

    adds GenericDeviceActionCard component

---
+++ components/GenericDeviceActionCard.vue
@@ -0,0 +1,123 @@
+<!--
+Web client of the Sensor Management System software developed within the
+Helmholtz DataHub Initiative by GFZ and UFZ.
+
+Copyright (C) 2020, 2021
+- Nils Brinckmann (GFZ, nils.brinckmann@gfz-potsdam.de)
+- Marc Hanisch (GFZ, marc.hanisch@gfz-potsdam.de)
+- Helmholtz Centre Potsdam - GFZ German Research Centre for
+  Geosciences (GFZ, https://www.gfz-potsdam.de)
+
+Parts of this program were developed within the context of the
+following publicly funded projects or measures:
+- Helmholtz Earth and Environment DataHub
+  (https://www.helmholtz.de/en/research/earth_and_environment/initiatives/#h51095)
+
+Licensed under the HEESIL, Version 1.0 or - as soon they will be
+approved by the "Community" - subsequent versions of the HEESIL
+(the "Licence").
+
+You may not use this work except in compliance with the Licence.
+
+You may obtain a copy of the Licence at:
+https://gitext.gfz-potsdam.de/software/heesil
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the Licence is distributed on an "AS IS" basis,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the Licence for the specific language governing
+permissions and limitations under the Licence.
+-->
+<template>
+  <v-card>
+    <v-card-subtitle class="pb-0">
+      {{ value.beginDate | toUtcDate }} - {{ value.endDate | toUtcDate }}
+    </v-card-subtitle>
+    <v-card-title class="pt-0">
+      {{ value.actionTypeName }}
+    </v-card-title>
+    <v-card-subtitle class="pb-1">
+      <v-row
+        no-gutters
+      >
+        <v-col
+          cols="11"
+        >
+          {{ value.contact.toString() }}
+        </v-col>
+        <v-col
+          align-self="end"
+          class="text-right"
+        >
+          <v-btn
+            icon
+            @click.stop.prevent="show()"
+          >
+            <v-icon>{{ isShown() ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card-subtitle>
+    <v-expand-transition>
+      <div
+        v-show="isShown(value.id)"
+      >
+        <v-card-text
+          class="grey lighten-5 text--primary pt-2"
+        >
+          <label>Description</label>
+          {{ value.description }}
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script lang="ts">
+/**
+ * @file provides a component for a Generic Device Actions form
+ * @author <marc.hanisch@gfz-potsdam.de>
+ */
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+import { DateTime } from 'luxon'
+import { dateToDateTimeString } from '@/utils/dateHelper'
+import { GenericDeviceAction } from '@/models/GenericDeviceAction'
+
+const toUtcDate = (date: DateTime): string => {
+  return dateToDateTimeString(date)
+}
+
+/**
+ * A class component for Generic Device Action card
+ * @extends Vue
+ */
+@Component({
+  filters: {
+    toUtcDate
+  }
+})
+// @ts-ignore
+export default class GenericDeviceActionCard extends Vue {
+  private showDetails: boolean = false
+
+  /**
+   * a GenericDeviceAction
+   */
+  @Prop({
+    default: () => new GenericDeviceAction(),
+    required: true,
+    type: Object
+  })
+  // @ts-ignore
+  readonly value!: GenericDeviceAction
+
+  isShown (): boolean {
+    return this.showDetails
+  }
+
+  show (): void {
+    this.showDetails = !this.showDetails
+  }
+}
+</script>
