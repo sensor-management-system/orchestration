8ba66c477b759b40ef64a9313d3fec2661ab75e0
Marc Hanisch <mhanisch@gfz-potsdam.de>
2021-05-07 14:30:29 +0200

    corrects comments

--- components/GenericDeviceActionForm.vue
+++ components/GenericDeviceActionForm.vue
@@ -123,6 +123,7 @@ export default class GenericDeviceActionForm extends Vue {
   readonly attachments!: Attachment[]
 
   created () {
+    // create a copy of the original value on which all operations will be applied
     this.actionCopy = GenericDeviceAction.createFromObject(this.value)
   }
 
@@ -134,6 +135,11 @@ export default class GenericDeviceActionForm extends Vue {
     this.$emit('input', value)
   }
 
+  /**
+   * sets the start date and validates start- and enddate
+   *
+   * @param {DateTime | null} aDate - the start date
+   */
   setStartDateAndValidate (aDate: DateTime | null) {
     this.actionCopy.beginDate = aDate
     if (this.actionCopy.endDate !== null) {
@@ -142,6 +148,11 @@ export default class GenericDeviceActionForm extends Vue {
     this.$emit('input', this.actionCopy)
   }
 
+  /**
+   * sets the end date and validates start- and enddate
+   *
+   * @param {DateTime | null} aDate - the end date
+   */
   setEndDateAndValidate (aDate: DateTime | null) {
     this.actionCopy.endDate = aDate
     if (this.actionCopy.beginDate !== null) {
@@ -150,10 +161,20 @@ export default class GenericDeviceActionForm extends Vue {
     this.$emit('input', this.actionCopy)
   }
 
+  /**
+   * validates the form based on its rules
+   *
+   */
   checkValidationOfDates () {
     return (this.$refs.datesForm as Vue & { validate: () => boolean }).validate()
   }
 
+  /**
+   * a rule to validate the start date
+   *
+   * @param {string} v - a string representation of the date as supplied by the datepicker component
+   * @return {boolean | string} whether the date is valid or an error message
+   */
   validateInputForStartDate (v: string): boolean | string {
     // NOTE: as the internals of the DatePicker component work with strings,
     // the validation functions should expect strings, too
@@ -169,6 +190,12 @@ export default class GenericDeviceActionForm extends Vue {
     return 'Start date must not be after end date'
   }
 
+  /**
+   * a rule to validate the end date
+   *
+   * @param {string} v - a string representation of the date as supplied by the datepicker component
+   * @return {boolean | string} whether the date is valid or an error message
+   */
   validateInputForEndDate (v: string): boolean | string {
     // NOTE: as the internals of the DatePicker component work with strings,
     // the validation functions should expect strings, too
@@ -184,6 +211,12 @@ export default class GenericDeviceActionForm extends Vue {
     return 'End date must not be before start date'
   }
 
+  /**
+   * a rule to check that an field is non-empty
+   *
+   * @param {string} fieldname - the (human readable) label of the field
+   * @return {(v: any) => boolean | string} a function that checks whether the field is valid or an error message
+   */
   mustBeProvided (fieldname: string): (v: any) => boolean | string {
     const innerFunc: (v: any) => boolean | string = function (v: any) {
       if (v == null || v === '') {
@@ -194,6 +227,10 @@ export default class GenericDeviceActionForm extends Vue {
     return innerFunc
   }
 
+  /**
+   * checks if the form is valid
+   *
+   */
   isValid (): boolean {
     return this.checkValidationOfDates() && (this.$refs.commonForm as Vue & { isValid: () => boolean }).isValid()
   }
