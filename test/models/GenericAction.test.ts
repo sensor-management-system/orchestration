ba33ff1e7b6933dc612776967f3f17e7b4349423
Marc Hanisch <mhanisch@gfz-potsdam.de>
2021-05-27 10:39:16 +0200

    adds test for GenericAction class

---
+++ test/models/GenericAction.test.ts
@@ -0,0 +1,74 @@
+/**
+ * @license
+ * Web client of the Sensor Management System software developed within
+ * the Helmholtz DataHub Initiative by GFZ and UFZ.
+ *
+ * Copyright (C) 2020
+ * - Nils Brinckmann (GFZ, nils.brinckmann@gfz-potsdam.de)
+ * - Marc Hanisch (GFZ, marc.hanisch@gfz-potsdam.de)
+ * - Helmholtz Centre Potsdam - GFZ German Research Centre for
+ *   Geosciences (GFZ, https://www.gfz-potsdam.de)
+ *
+ * Parts of this program were developed within the context of the
+ * following publicly funded projects or measures:
+ * - Helmholtz Earth and Environment DataHub
+ *   (https://www.helmholtz.de/en/research/earth_and_environment/initiatives/#h51095)
+ *
+ * Licensed under the HEESIL, Version 1.0 or - as soon they will be
+ * approved by the "Community" - subsequent versions of the HEESIL
+ * (the "Licence").
+ *
+ * You may not use this work except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * https://gitext.gfz-potsdam.de/software/heesil
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+import { DateTime } from 'luxon'
+import { Attachment } from '@/models/Attachment'
+import { Contact } from '@/models/Contact'
+import { GenericAction } from '@/models/GenericAction'
+
+describe('GenericAction', () => {
+  test('create a GenericAction from an object', () => {
+    const date1 = DateTime.fromISO('2021-05-27')
+    const date2 = DateTime.fromISO('2021-05-28')
+
+    const attachment = new Attachment()
+    attachment.id = '1'
+    attachment.label = 'an attachment'
+    attachment.url = 'https://foo/baz'
+
+    const contact = new Contact()
+    contact.givenName = 'Homer'
+    contact.familyName = 'Simpson'
+    contact.email = 'homer.simpson@springfield.com'
+
+    const action = GenericAction.createFromObject({
+      id: '1',
+      description: 'This is a generic action description',
+      actionTypeName: 'Generic Device Action',
+      actionTypeUrl: 'https://foo/bar',
+      beginDate: date1,
+      endDate: date2,
+      contact,
+      attachments: [attachment]
+    })
+
+    expect(typeof action).toBe('object')
+    expect(action).toHaveProperty('id', '1')
+    expect(action).toHaveProperty('description', 'This is a generic action description')
+    expect(action).toHaveProperty('actionTypeName', 'Generic Device Action')
+    expect(action).toHaveProperty('actionTypeUrl', 'https://foo/bar')
+    expect(action.beginDate).toBe(date1)
+    expect(action.endDate).toBe(date2)
+    expect(action.contact).toStrictEqual(contact)
+    expect(action.attachments).toContain(attachment)
+    expect(action.isGenericAction).toBeTruthy()
+  })
+})
