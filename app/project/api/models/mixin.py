af77106a727cd81dff939934ea92b88beb30664d
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2021-06-09 14:37:41 +0200

    Mixin class for indirect searchable entries

--- app/project/api/models/mixin.py
+++ app/project/api/models/mixin.py
@@ -55,6 +55,35 @@ class AuditMixin:
             remote_side="User.id",
         )
 
+class IndirectSearchableMixin:
+    """
+    Mixin for chose elements that should only be indirect searchable.
+
+    An example: The device properties that are part of the devices.
+    We will only search directly for devices - but the full text
+    index should contain up to date information about the device
+    properties as well.
+
+    This is what this IndirectSearchableMixin should be used for.
+    """
+
+    def get_parent_search_entities(self):
+        """
+        Return the list of "parent" search entities.
+
+        As an example for a device property I want to
+        have the device - as the device property will be
+        included in the full text search index entry for the
+        device.
+        However for contacts, we want to have them included in
+        multiple entries (devices, platforms, configurations), so
+        we need to handle a list of those entries.
+
+        Please also note: We only have a very small set of
+        full searchable entities for the moment (devices, platforms,
+        configurations & contacts).
+        """
+        return []
 
 class SearchableMixin:
     """Mixin to make a model searchable via full text search."""
@@ -96,7 +125,7 @@ class SearchableMixin:
         session._search_add = []
 
         for obj in itertools.chain(session._changes["add"], session._changes["update"]):
-            if isinstance(obj, SearchableMixin):
+            if isinstance(obj, SearchableMixin) or isinstance(obj, IndirectSearchableMixin):
                 session._search_add.append(
                     SearchModelWithEntry(model=obj, entry=obj.to_search_entry())
                 )
@@ -105,14 +134,44 @@ class SearchableMixin:
     def after_commit(cls, session):
         """Update the search after the sqlalchemy commit."""
         ids_to_add = collections.defaultdict(set)
+
+        # We are going to collect all the ids for which we need to add or update
+        # the search index.
+        # Adding & Updating is here the very same command, so we can handle
+        # all of them in one run.
+
+        # When we go through all of the changes to add & to update
+        # then we can take all the direct searchable entities, as well
+        # as all parents of the indirect ones.
         for obj in itertools.chain(session._changes["add"], session._changes["update"]):
-            if isinstance(obj, SearchableMixin):
+            if isinstance(obj, IndirectSearchableMixin):
+                for parent_obj in obj.get_parent_search_entities():
+                    if isinstance(parent_obj, SearchableMixin):
+                        ids_to_add[obj.__tablename__].add(obj.id)
+            elif isinstance(obj, SearchableMixin):
                 ids_to_add[obj.__tablename__].add(obj.id)
+        # For those indirect ones we also want to check the deleted ones
+        # So that we know that the parents must be updated as well.
+        for obj in session._changes["delete"]:
+            if isinstance(obj, IndirectSearchableMixin):
+                for parent_obj in obj.get_parent_search_entities():
+                    if isinstance(parent_obj, SearchableMixin):
+                        ids_to_add[parent_obj.__tablename__].add(obj.id)
+        # However, if the ones that we want to update get deleted themselves
+        # it really doesn't make any more sense to update them in the search
+        # index. We can ignore them right away.
+        for obj in session._changes["delete"]:
+            if isinstance(obj, SearchableMixin):
+                ids_to_add[obj.__tablename__].remove(obj.id)
+
+        # So, now we have all of those that we want to update,
+        # we can start adding them to the search index.
         for search_model_with_entry in session._search_add:
             model = search_model_with_entry.model
             if model.id in ids_to_add[model.__tablename__]:
                 add_to_index(model.__tablename__, model, search_model_with_entry.entry)
 
+        # And then, we can delete old entries if necessary.
         for obj in session._changes["delete"]:
             if isinstance(obj, SearchableMixin):
                 remove_from_index(obj.__tablename__, obj)
