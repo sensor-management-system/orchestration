cb2d1e83c0191a772298bc11d20de14436e191fb
Tobias Kuhnert <tobias.kuhnert@ufz.de>
2021-07-06 13:46:00 +0200

    added methods

--- pages/platforms/_platformId/actions.vue
+++ pages/platforms/_platformId/actions.vue
@@ -23,21 +23,73 @@
     </template>
 
     <template v-else>
-      <platform-action-timeline :actions="actions" />
+      <platform-action-timeline
+        :actions="actions"
+        :platform-id="platformId"
+      />
       <platform-action-delete-dialog />
     </template>
   </div>
 </template>
 
-<script>
-import PlatformActionTimeline from "@/components/platform/actions/PlatformActionTimeline";
-import PlatformNewActionButton from "@/components/platform/actions/PlatformNewActionButton";
-import ProgressIndicator from "@/components/ProgressIndicator";
-import PlatformActionDeleteDialog from "@/components/platform/actions/PlatformActionDeleteDialog";
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import PlatformActionTimeline from '@/components/platform/actions/PlatformActionTimeline'
+import PlatformNewActionButton from '@/components/platform/actions/PlatformNewActionButton'
+import ProgressIndicator from '@/components/ProgressIndicator'
+import PlatformActionDeleteDialog from '@/components/platform/actions/PlatformActionDeleteDialog'
+import { GenericAction } from '@/models/GenericAction'
+import { IActionCommonDetails } from '@/models/ActionCommonDetails'
 
-export default {
-  name: "action",
-  components: {PlatformActionDeleteDialog, PlatformNewActionButton, PlatformActionTimeline, ProgressIndicator}
+@Component({
+  components: {
+    PlatformActionTimeline,
+    PlatformNewActionButton,
+    ProgressIndicator,
+    PlatformActionDeleteDialog
+  }
+})
+export default class PlatformActionsPage extends Vue {
+  private isSaving: boolean = false
+  private isLoading: boolean = false
+  private actions: IActionCommonDetails[] = []
+
+  get platformId (): string {
+    return this.$route.params.platformId
+  }
+
+  get isInProgress (): boolean {
+    return this.isLoading || this.isSaving
+  }
+
+  get isAddActionPage (): boolean {
+    // eslint-disable-next-line no-useless-escape
+    const addUrl = '^\/platforms\/' + this.platformId + '\/actions\/new$'
+    return !!this.$route.path.match(addUrl)
+  }
+
+  get isEditActionPage (): boolean {
+    // eslint-disable-next-line no-useless-escape
+    const editUrl = '^\/devices\/' + this.platformId + '\/actions\/[a-zA-Z-]+\/[0-9]+\/edit$'
+    return !!this.$route.path.match(editUrl)
+  }
+
+  showsave (isSaving: boolean) {
+    this.isSaving = isSaving
+  }
+
+  showload (isLoading: boolean) {
+    this.isLoading = isLoading
+  }
+
+  async fetchGenericActions (): Promise<void> {
+    const actions: GenericAction[] = await this.$api.platforms.findRelatedGenericActions(this.platformId)
+    actions.forEach((action: GenericAction) => this.actions.push(action))
+  }
+
+  async fetch () {
+    await this.fetchGenericActions()
+  }
 }
 </script>
 
