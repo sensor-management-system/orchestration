71dd6522a9a6b0854fefb86ceb8d1b3776eac0c2
alhajtah <kotyba.alhaj-taha@ufz.de>
2021-07-29 10:44:17 +0200

    Update the nginx and use only one env file.

---
+++ app/project/api/helpers/health_checks.py
@@ -0,0 +1,49 @@
+from subprocess import check_output
+
+import requests
+from sqlalchemy.exc import SQLAlchemyError
+
+from ..models.base_model import db
+
+
+def health_check_elastic_search():
+    """
+    Check health status of elastic search server
+    :return:
+    """
+    try:
+        _ = requests.get('http://smses01:9200').content
+        return True, "elastic search ok"
+
+    except requests.exceptions.HTTPError:
+        return False, "Error connecting to elastic search server"
+
+
+def health_check_db():
+    """
+    Check health status of db
+    :return:
+    """
+    try:
+        db.session.execute('SELECT 1')
+        return True, 'database ok'
+    except SQLAlchemyError:
+
+        return False, 'Error connecting to database'
+
+
+def health_check_migrations():
+    """
+    Checks whether database is up to date with migrations.
+    :return:
+    """
+    head = check_output(["python", "manage.py", "db", "current"]).decode("utf-8").split(" ")[0]
+    print(head)
+
+    version_num = (db.session.execute('SELECT version_num from alembic_version').fetchone())[
+        'version_num']
+    print(version_num)
+    if head == version_num:
+        return True, 'database up to date with migrations'
+    return False, 'database out of date with migrations'
+
