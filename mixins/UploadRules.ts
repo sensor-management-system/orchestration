3b61766c9131704aaf8b39df35a0c27cac05fe2c
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2021-08-10 14:42:42 +0200

    This adds the upload functionality for attachments in the frontend

---
+++ mixins/UploadRules.ts
@@ -0,0 +1,73 @@
+/**
+ * @license
+ * Web client of the Sensor Management System software developed within
+ * the Helmholtz DataHub Initiative by GFZ and UFZ.
+ *
+ * Copyright (C) 2020-2021
+ * - Nils Brinckmann (GFZ, nils.brinckmann@gfz-potsdam.de)
+ * - Marc Hanisch (GFZ, marc.hanisch@gfz-potsdam.de)
+ * - Helmholtz Centre Potsdam - GFZ German Research Centre for
+ *   Geosciences (GFZ, https://www.gfz-potsdam.de)
+ *
+ * Parts of this program were developed within the context of the
+ * following publicly funded projects or measures:
+ * - Helmholtz Earth and Environment DataHub
+ *   (https://www.helmholtz.de/en/research/earth_and_environment/initiatives/#h51095)
+ *
+ * Licensed under the HEESIL, Version 1.0 or - as soon they will be
+ * approved by the "Community" - subsequent versions of the HEESIL
+ * (the "Licence").
+ *
+ * You may not use this work except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * https://gitext.gfz-potsdam.de/software/heesil
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+
+/**
+ * @file provides a mixin component for standard form validation rules
+ * @author <marc.hanisch@gfz-potsdam.de>
+ */
+
+import { Vue, Component } from 'nuxt-property-decorator'
+import UploadConfig from '@/config/uploads'
+
+/**
+ * A mixin component for standard form validation rules for uploads
+ * @extends Vue
+ */
+@Component
+export class UploadRules extends Vue {
+  /**
+   * various rules for validating form inputs
+   *
+   */
+  private uploadRules: Object = {
+    maxSize: (f: File | null) => {
+      if (f !== null && f.size > this.$api.upload.UPLOAD_SIZE_LIMIT) {
+        return 'File is too large'
+      }
+      return true
+    },
+    mimeTypeAllowed: (f: File | null) => {
+      if (f === null) {
+        return true
+      }
+
+      const mimeTypeIndex = UploadConfig.allowedMimeTypes.indexOf(f.type)
+      if (mimeTypeIndex < 0) {
+        if (f.type) {
+          return '' + f.type + ' is not supported'
+        }
+        return 'Unsupported file type'
+      }
+      return true
+    }
+  }
+}
