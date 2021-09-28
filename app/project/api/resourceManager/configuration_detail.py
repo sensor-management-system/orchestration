5582ab01f937c2fc163b8167c3760c64323c0313
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2021-09-28 09:59:25 +0200

    Resolve "Attachments: Only delete objects from minio that are no longer referenced in the sms (Follow-up from "imporove exception handling")"

--- app/project/api/resourceManager/configuration_detail.py
+++ app/project/api/resourceManager/configuration_detail.py
@@ -1,5 +1,8 @@
-from flask_rest_jsonapi import ResourceDetail
+from flask_rest_jsonapi import ResourceDetail, JsonApiException
+from flask_rest_jsonapi.exceptions import ObjectNotFound
 
+from .base_resource import delete_attachments_in_minio_by_url
+from ..helpers.errors import ConflictError
 from ..models.base_model import db
 from ..models.configuration import Configuration
 from ..resourceManager.base_resource import add_updated_by_id
@@ -16,6 +19,30 @@ class ConfigurationDetail(ResourceDetail):
     def before_patch(self, args, kwargs, data):
         """Add Created by user id to the data"""
         add_updated_by_id(data)
+
+    def delete(self, *args, **kwargs):
+        """
+        Try to delete an object through sqlalchemy. If could not be done give a ConflictError.
+        :param args: args from the resource view
+        :param kwargs: kwargs from the resource view
+        :return:
+        """
+        configuration = (
+            db.session.query(Configuration).filter_by(id=kwargs["id"]).first()
+        )
+        if configuration is None:
+            raise ObjectNotFound({"pointer": ""}, "Object Not Found")
+        urls = [a.url for a in configuration.configuration_attachments]
+        try:
+            super().delete(*args, **kwargs)
+        except JsonApiException as e:
+            raise ConflictError("Deletion failed for the configuration.", str(e))
+
+        for url in urls:
+            delete_attachments_in_minio_by_url(url)
+
+        final_result = {"meta": {"message": "Object successfully deleted"}}
+        return final_result
 
     schema = ConfigurationSchema
     decorators = (token_required,)
--- app/project/api/resourceManager/device_detail.py
+++ app/project/api/resourceManager/device_detail.py
@@ -1,5 +1,8 @@
-from flask_rest_jsonapi import ResourceDetail
+from flask_rest_jsonapi import ResourceDetail, JsonApiException
+from flask_rest_jsonapi.exceptions import ObjectNotFound
 
+from .base_resource import delete_attachments_in_minio_by_url
+from ..helpers.errors import ConflictError
 from ..models.base_model import db
 from ..models.device import Device
 from ..resourceManager.base_resource import add_updated_by_id
@@ -16,6 +19,28 @@ class DeviceDetail(ResourceDetail):
     def before_patch(self, args, kwargs, data):
         """Add Created by user id to the data"""
         add_updated_by_id(data)
+
+    def delete(self, *args, **kwargs):
+        """
+        Try to delete an object through sqlalchemy. If could not be done give a ConflictError.
+        :param args: args from the resource view
+        :param kwargs: kwargs from the resource view
+        :return:
+        """
+        device = db.session.query(Device).filter_by(id=kwargs["id"]).first()
+        if device is None:
+            raise ObjectNotFound({"pointer": ""}, "Object Not Found")
+        urls = [a.url for a in device.device_attachments]
+        try:
+            super().delete(*args, **kwargs)
+        except JsonApiException as e:
+            raise ConflictError("Deletion failed for the device.", str(e))
+
+        for url in urls:
+            delete_attachments_in_minio_by_url(url)
+
+        final_result = {"meta": {"message": "Object successfully deleted"}}
+        return final_result
 
     schema = DeviceSchema
     decorators = (token_required,)
