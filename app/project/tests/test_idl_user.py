52079c910cf99dcf317b75a600e58830d4792690
alhajtah <kotyba.alhaj-taha@ufz.de>
2021-10-19 12:28:03 +0200

    Fix the tests

--- app/project/tests/test_idl_user.py
+++ app/project/tests/test_idl_user.py
@@ -6,7 +6,8 @@ from project.tests.base import BaseTestCase
 
 class TestIdlUserGroups(BaseTestCase):
     def test_idl_from_dict(self):
-        json_string = '[{"id": 1,"username": "testuser@ufz.de","displayName": "Test User (WKDV)","referencedIri": "/infra/api/v1/accounts/testuser","administratedPermissionsGroups": [2],"memberedPermissionsGroups": [1,3]}]'
+        json_string = '[{"id": 1,"username": "testuser@ufz.de","displayName": "Test User (WKDV)","referencedIri": \
+        "/infra/api/v1/accounts/testuser","administratedPermissionsGroups": [2],"memberedPermissionsGroups": [1,3]}]'
         result = idl_from_dict(json.loads(json_string))[0]
         assert "testuser@ufz.de" == result.username
         assert 2 in result.administrated_permissions_groups
