cfa42ce4399e350f7654e00fe1db130b9b7180fe
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2021-11-08 12:20:32 +0100

    Improved comments

--- docker/build/mc/gfz/projectshare.sh
+++ docker/build/mc/gfz/projectshare.sh
@@ -8,13 +8,13 @@
 for SERVICE in backend-db vocabulary-db minio
 do
 
-    # We must make sure that our target folder exists
+    # We must make sure that our target folder exists.
     mkdir -p /mnt/sms-backup/${SERVICE}
     # Then we run a little cleanup for target folder
-    # (remove files older then cleanup days)
+    # (remove files older then cleanup days).
     find /mnt/sms-backup/${SERVICE} -type f -mtime +${CLEANUP:-180} -exec rm {} \;
-    # We want to know that our oldest file is
+    # We want to know what our latest file is.
     LATEST_DUMP=$(ls -Art /srv/docker/service/${SERVICE}/backups | tail -n 1)
-    # And then we copy it over to our target folder
+    # And then we copy it over to our target folder.
     cp -r /srv/docker/service/${SERVICE}/backups/${LATEST_DUMP} /mnt/sms-backup/${SERVICE}/${LATEST_DUMP}
 done
