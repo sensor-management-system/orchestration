8b88be9d10f8f7042eaa600f731c57482be60ef3
Tobias Kuhnert <tobias.kuhnert@ufz.de>
2022-04-20 11:52:06 +0200

    added basic search component for platforms

---
+++ components/platforms/PlatformsBasicSearch.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-tab-item :eager="true">
+    <v-row>
+      <v-col cols="12" md="5">
+        <PlatformsBasicSearchField
+          v-model="searchText"
+          @start-search="emitSearch"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        md="7"
+        align-self="center"
+      >
+        <v-btn
+          color="primary"
+          small
+          @click="emitSearch"
+        >
+          Search
+        </v-btn>
+        <v-btn
+          text
+          small
+          @click="clearSearch"
+        >
+          Clear
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-tab-item>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import PlatformsBasicSearchField from '@/components/platforms/PlatformsBasicSearchField.vue'
+import { Prop } from 'nuxt-property-decorator'
+
+@Component({
+  components: { PlatformsBasicSearchField }
+})
+export default class PlatformsBasicSearch extends Vue {
+  @Prop({
+    required: true,
+  })
+  readonly value!: string | null
+
+  get searchText () {
+    return this.value
+  }
+
+  set searchText (newVal) {
+    this.$emit('input', newVal)
+  }
+
+  emitSearch () {
+    this.$emit('search')
+  }
+
+  clearSearch () {
+    this.searchText = null
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/platforms/PlatformsBasicSearchField.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-text-field
+    v-model="text"
+    label="Name"
+    placeholder="Name of platform"
+    hint="Please enter at least 3 characters"
+    @keydown.enter="emitKeydown"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class PlatformsBasicSearchField extends Vue {
+  @Prop({
+    required: true,
+  })
+  readonly value!: string|null
+
+  get text(){
+    return this.value;
+  }
+
+  set text(newVal){
+    this.$emit('input',newVal)
+  }
+  emitKeydown(){
+    this.$emit('start-search');
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
