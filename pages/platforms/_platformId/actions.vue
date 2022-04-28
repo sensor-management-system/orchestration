1c72bd84f91f78c5dd6dd1f0cd3f7f858227d87a
Tobias Kuhnert <tobias.kuhnert@ufz.de>
2022-04-28 10:40:08 +0200

    updated platforms actions

--- pages/platforms/_platformId/actions.vue
+++ pages/platforms/_platformId/actions.vue
@@ -33,58 +33,45 @@ implied. See the Licence for the specific language governing
 permissions and limitations under the Licence.
 -->
 <template>
-      <NuxtChild/>
+  <div>
+    <ProgressIndicator
+      v-model="isLoading"
+    />
+    <NuxtChild/>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import { mapActions } from 'vuex'
+import ProgressIndicator from '@/components/ProgressIndicator.vue'
 
 @Component({
+  components: { ProgressIndicator },
   methods:mapActions('platforms',['loadAllPlatformActions'])
 })
 export default class PlatformActionsPage extends Vue {
-  async created(){
-    await this.loadAllPlatformActions(this.platformId);
-  }
-  // async fetch () {
-  //   this.showload(true)
-  //   await this.fetchActions()
-  //   this.showload(false)
-  // }
+  private isLoading = false
 
-  // async fetchActions (): Promise<void> {
-  //   this.actions = []
-  //   await Promise.all([
-  //     this.fetchGenericActions(),
-  //     this.fetchSoftwareUpdateActions(),
-  //     this.fetchMountActions(),
-  //     this.fetchUnmountActions()
-  //   ])
-  //   // sort the actions
-  //   const comparator = new DateComparator()
-  //   this.actions.sort((i: IActionCommonDetails, j: IActionCommonDetails): number => {
-  //     if (isDateCompareable(i) && isDateCompareable(j)) {
-  //       // multiply result with -1 to get descending order
-  //       return comparator.compare(i, j) * -1
-  //     }
-  //     if (isDateCompareable(i)) {
-  //       return -1
-  //     }
-  //     if (isDateCompareable(j)) {
-  //       return 1
-  //     }
-  //     return 0
-  //   })
-  // }
-  head () {
-    return {
-      titleTemplate: 'Actions - %s'
+  async created(){
+    try {
+      this.isLoading=true
+      await this.loadAllPlatformActions(this.platformId)
+    } catch (e) {
+      this.$store.commit('snackbar/setError', 'Failed to fetch actions')
+    } finally {
+      this.isLoading=false
     }
   }
 
   get platformId (): string {
     return this.$route.params.platformId
+  }
+
+  head () {
+    return {
+      titleTemplate: 'Actions - %s'
+    }
   }
 }
 </script>
