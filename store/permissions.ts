76b8b61438fb4670736fb041d710e4629a621d5b
Tobias Kuhnert <tobias.kuhnert@ufz.de>
2022-05-09 13:03:33 +0200

    added userinfo api

--- store/permissions.ts
+++ store/permissions.ts
@@ -1,12 +1,31 @@
-const state = () => ({
+import { Api } from '@/services/Api'
+import { Commit } from 'vuex'
+
+interface permissionsState {
+  userInfo:{}
+}
+  const state = ():permissionsState => ({
   userInfo:{}
 })
 
-const getters = {}
+const getters = {
 
-const actions = {}
+}
+// @ts-ignore
+const actions: {
+  [key: string]: any;
+  $api: Api
+} = {
+  loadUserInfo({commit}:{commit:Commit}){
 
-const mutations = {}
+  }
+}
+
+const mutations = {
+  setUserInfo(state:permissionsState,userInfo:any){
+    state.userInfo = userInfo
+  }
+}
 
 export default {
   namespaced: true,
