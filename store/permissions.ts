e2a602e1e8818a37a2bce73c0d760ccaa46ad9cd
Tobias Kuhnert <tobias.kuhnert@ufz.de>
2022-05-09 08:44:40 +0200

    created new store module

---
+++ store/permissions.ts
@@ -0,0 +1,17 @@
+const state = () => ({
+  userInfo:{}
+})
+
+const getters = {}
+
+const actions = {}
+
+const mutations = {}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
