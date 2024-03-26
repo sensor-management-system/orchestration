f7f4656cd6608b4ca9fa576cdaf76cbd58290bba
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2024-03-26 13:28:54 +0100

    Fix typo on tsm linking wizzard

--- frontend/components/configurations/tsmLinking/TsmLinkingWizard.vue
+++ frontend/components/configurations/tsmLinking/TsmLinkingWizard.vue
@@ -2,7 +2,7 @@
  Web client of the Sensor Management System software developed within the
  Helmholtz DataHub Initiative by GFZ and UFZ.
 
- Copyright (C) 2020 - 2023
+ Copyright (C) 2020 - 2024
  - Nils Brinckmann (GFZ, nils.brinckmann@gfz-potsdam.de)
  - Marc Hanisch (GFZ, marc.hanisch@gfz-potsdam.de)
  - Helmholtz Centre Potsdam - GFZ German Research Centre for
@@ -62,7 +62,7 @@
         step="2"
         :editable="stepTwoIsSelectable"
       >
-        Select measured quanitites
+        Select measured quantities
         <small>Select one or multiple measured quantities to link.</small>
       </v-stepper-step>
 
