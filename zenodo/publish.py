998b25c08ecd2cf70a73d7ea1038cc25d0066a6d
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2024-09-02 15:50:47 +0200

    Use right license (eupl)

--- zenodo/publish.py
+++ zenodo/publish.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-# SPDX-FileCopyrightText: 2024 - 2024
+# SPDX-FileCopyrightText: 2024
 # - Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
 # - Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences (GFZ, https://www.gfz-potsdam.de)
 #
-# SPDX-License-Identifier: HEESIL-1.0
+# SPDX-License-Identifier: EUPL-1.2
 
 """Publish the SMS on zenodo."""
 
