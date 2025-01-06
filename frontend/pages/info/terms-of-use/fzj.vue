a7630924c78aa11863cba5e9c9ccd80908b9eadb
Rubankumar Moorthy <r.moorthy@fz-juelich.de>
2025-01-06 19:27:48 +0100

    Resolve "FZJ: Update the footer section  content for Prod instance"

--- frontend/pages/info/terms-of-use/fzj.vue
+++ frontend/pages/info/terms-of-use/fzj.vue
@@ -1,5 +1,5 @@
 <!--
-SPDX-FileCopyrightText: 2020 - 2023
+SPDX-FileCopyrightText: 2020 - 2024
 - Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
 - Marc Hanisch <marc.hanisch@gfz-potsdam.de>
 - Rubankumar Moorthy <r.moorthy@fz-juelich.de>
@@ -15,33 +15,284 @@ SPDX-License-Identifier: EUPL-1.2
         Terms of use for Sensor Management System
       </h1>
       <p class="block">
-        This version of the sensor management system is provided as a demonstration for testing purposes only. Users are granted access to explore and evaluate the functionalities and user experience.
+        By using the offered services at <a href="https://sms.earth-data.fz-juelich.de">sms.earth-data.fz-juelich.de</a>
+        (“Services”) you agree to the following terms of use of Forschungszentrum Jülich (“FZJ”), a Public Law Foundation under the laws of the Federal State of
+        North Rhine-Westphalia/Germany, Wilhelm-Johnen-Straße, 52428 Jülich, Germany.
+        Mail: <a href="mailto:ibg3sms@fz-juelich.de">ibg3sms@fz-juelich.de</a>
       </p>
     </section>
     <section class="mb-6">
       <h3 class="mb-1">
-        I. Usage guidelines:
+        I. Service Sensor Management System – what it is about
       </h3>
-      <ul>
-        <li>This is a demo version intended solely for testing and evaluation purposes.</li>
-        <li>The system may contain limited features and data.</li>
-        <li>Users can experiment with functionality but should not rely on the data or system stability.</li>
-        <li>Any data entered or generated within the demo version may be reset or deleted without notice.</li>
-        <li>Users are prohibited from using this demo version for production</li>
-      </ul>
-
-      <h4 class="mb-1">
-        II. Disclaimer:
-      </h4>
+      <p class="block">
+        The Services of the Sensor Management System (“SMS”) encompass the provision of a database
+        with data and information on sensors, measurement setups and campaigns run in scientific
+        context provided by FZJ and third parties (“Content”).
+      </p>
+      <p class="block">
+        The SMS can be used by individuals without registration as visit to the website
+        (“Visitors”, see III.) and by individuals who have registered an account and who also can
+        upload Content (“Registered User”, see II.).
+      </p>
+      <p class="block">
+        The Content is only provided for information purposes. If applicable, Registered Users
+        can add information where further information on the conditions for renting or borrowing
+        a sensor can be found. It is not possible to rent or borrow a sensor via the SMS.
+        A commercial use of the SMS is explicitly forbidden.
+      </p>
+    </section>
+    <section class="mb-6">
+      <h3 class="mb-1">
+        II. Provision of Content as Registered User
+      </h3>
+      <p class="block">
+        To be able to add, modify, recreate and store Content to the SMS as “Registered User” ,
+        You need a registered account at one of the identity providers registered at Helmholtz
+        Authentication and Authorization Infrastructure (“Helmholtz AAI”). In most of the cases
+        this account is provided by your user account at your home institution participating at
+        Helmholtz AAI.
+      </p>
+      <p class="block">
+        When creating Content, the Registered User must be a member of a virtual organization
+        (“Editing Group”) in the Helmholtz AAI.
+        See
+        <a href="https://hifis.net/doc/helmholtz-aai/howto-vo-management/">
+          https://hifis.net/doc/helmholtz-aai/howto-vo-management/
+        </a>
+        for more information.
+      </p>
+      <p class="block">
+        As Registered User, You grant us an irrevocable, transferable, sublicensable, worldwide
+        and perpetual right to use the Content in and in the context of the SMS. You also agree
+        that we and other Registered Users or Visitors of the SMS are entitled to make publicly
+        accessible, copy or distribute metadata provided by You under Creative Commons License CCO.
+        You and your Organization indemnify and hold us harmless from any costs, damages and other
+        losses we suffer as a result of third-party claims stating that the Content infringes such
+        third-party rights or is otherwise unlawful.
+      </p>
+      <p class="block">
+        The visibility of the Content can be classified by the Registered User or authorized
+        participants of the Editing Group for the specific Content as <i>private</i> (only visible to the
+        Registered User who created that specific Content, <i>internal</i> (only visible to Registered Users
+        of the SMS) and <i>public</i> (visible to Registered Users and Visitors).
+      </p>
+      <p class="block">
+        When You provide Content, in addition to the above the following applies:
+      </p>
       <p class="block">
         <ul>
-          <li>The demo version may not represent the final version's features, accuracy, or performance.</li>
-          <li>We do not guarantee the security, reliability, or availability of the system</li>
-          <li>All data inputted or generated within the demo version is done at the user's own risk.</li>
+          <li>You shall use the SMS in accordance with applicable laws and regulations;</li>
+          <li>You shall not knowingly place incorrect or otherwise misleading or unlawful Content in the SMS;</li>
+          <li>You shall not upload private content;</li>
+          <li>
+            You shall not upload content that breaches copyright regulations, personality rights,
+            or confidentiality undertakings signed in connection with your work;
+          </li>
+          <li>
+            You shall only include links to URLs and third party content that you are legally allowed to
+            do so and under observance of copyright issues (i.e. giving copyright attribution); in case you
+            receive the information that the third party URL or Content is not lawfully or not with
+            legal allowance included in your account, you shall delete it with immediate effect;
+          </li>
+          <li>
+            Neither you, nor anyone on your behalf shall use the SMS in a manner that would require us
+            and/or you to obtain an export control license in any country. Such use includes without limitation
+            training or use for weapons, whether biological, chemical, nuclear or otherwise and for any
+            other military or nuclear devices;
+          </li>
+          <li>
+            You are responsible for all activity occurring under your Account. If you believe your Account
+            has been, or can be, accessed by an unauthorized person, you should immediately change your password
+            and should notify us immediately by email at <a href="mailto:support@hifis.net">support@hifis.net</a>;
+          </li>
         </ul>
       </p>
       <p class="block">
-        By accessing and using this demo version, you agree to abide by these terms and conditions. Any unauthorized use beyond the scope of testing is prohibited.
+        The respective home institution will be notified in the event of any of these conditions being
+        breached. The institution will then decide on any legal action that may be necessary in addition
+        to legal steps we reserve.
+      </p>
+    </section>
+    <section class="mb-6">
+      <h3 class="mb-1">
+        III. General Conditions for Viewing SMS
+      </h3>
+      <h4 class="mb-2">
+        (1) Allowed Use
+      </h4>
+      <p class="block">
+        Only Registered Users can provide Content under the conditions as stated in II.
+      </p>
+      <p class="block">
+        Registered Users and Visitors can browse and view the Content. Visitors can only view
+        Content that has been published by Registered Users for public access (<i>public</i>).
+      </p>
+      <p class="block">
+        The SMS and its Content can be used for scientific, educational, informational and research purposes.
+      </p>
+      <p class="block">
+        When using the Services, you always have to observe the licenses of the third party material
+        as given by the third parties and, if applicable, give the according Copyright Contribution.
+        In case of doubt about the allowed use, you must contact the copyright holder directly.
+      </p>
+      <h4 class="mb-2">
+        (2) Unacceptable Use
+      </h4>
+      <p class="block">
+        The following uses of the Services are strictly forbidden and can lead to IP blocking when doing so:
+      </p>
+      <p class="block">
+        <ul>
+          <li>
+            heavy use of the Services from an application or individual person; examples for heavy use are:
+            using our Services as a fallback for your own services, more than 500 requests within one minute,
+            more than 5000 requests within one hour
+          </li>
+          <li>seeding or crawling the Services</li>
+          <li>sending the same request multiple times in a short amount of time</li>
+          <li>monitoring the Services in a periodically manner</li>
+          <li>bulk downloading, if not explicitly permitted (see next paragraph III. (3))</li>
+          <li>attempting to circumvent administrative authorisations and security mechanisms</li>
+        </ul>
+      </p>
+      <h4 class="mb-2">
+        (3) Bulk Downloading
+      </h4>
+      <p class="block">
+        As a general rule, bulk downloading of larger amounts of data is not allowed. Smaller one-time
+        bulk downloads may be possible if you ask the system administrators for permission to do so.
+        For those bulk downloads following additional rules apply:
+      </p>
+      <p class="block">
+        <ul>
+          <li>limit your request to a single thread;</li>
+          <li>request from one machine only, no distributed scripts (for example a cluster or cloud service);</li>
+          <li>results must be cached on your side, so clients sending repeatedly the same request may be blocked.</li>
+        </ul>
+      </p>
+    </section>
+    <section class="mb-6">
+      <h3 class="mb-1">
+        IV. Interface for Websites and Applications
+      </h3>
+      <p class="block">
+        (1) You can access the data or integrate the data via the interfaces provided by the SMS. You use the
+        Services at your own risk and under your own responsibility.
+      </p>
+      <p class="block">
+        (2) When calling the SMS from a web application or by programmatic script (like Shell, Python or R)
+        please be sure to provide a valid HTTP referer and/or user agent identifying the application or client.
+        The HTTP requests should contain at least one of the headers “Referer” or “User-Agent” as it is shown in
+        the following examples:
+      </p>
+      <p class="block">
+        <ul>
+          <li>in case of a request from a web application: Referer: example.org;</li>
+          <li>in case of a script (for example a request from cURL): User-Agent: curl/7.37.0.</li>
+        </ul>
+      </p>
+    </section>
+    <section class="mb-6">
+      <h3 class="mb-1">
+        V. Intellectual property rights to SMS, Citation
+      </h3>
+      <p class="block">
+        All intellectual property rights, including ‘sui generis’ database rights and the like related to the
+        SMS ("our intellectual property rights") remain ours or our licensors’.
+        For the avoidance of doubt: the foregoing does not include third-party content included in the SMS or
+        linked to via the SMS.
+      </p>
+      <p class="block">
+        You are granted a revocable, non-transferable, non-sublicensable, limited right to use the SMS for the
+        purposes set out in these Terms of Use, we reserve all other rights.
+      </p>
+      <p class="block">
+        You are not allowed to grant or transfer the right granted to you to any others, or make copies – or
+        parts of it – of the SMS, or to sell, exploit, transfer, lease, distribute, rent it or otherwise dispose
+        of the SMS, or grant other any other any rights thereto without our prior consent.
+        You may not make changes to the SMS, except of course when you are adding Content during your use of the SMS.
+      </p>
+      <p class="block">
+        If You use the Services and/or data sets in a presentation or publication, for instance by linking in
+        your website, then You have to acknowledge the SMS as the source of the material.
+        The Content can be cited as defined by the copyright attributions of the metadata of Content.
+        To cite our Service, please reference us as <code>{{ citation }}</code>.
+        You may not use the Services in a way that suggests any official status or that FZJ endorses any
+        particular application of the SMS by You. You may not mislead others or misrepresent the Services
+        and its sources.
+      </p>
+    </section>
+    <section class="mb-6">
+      <h3 class="mb-1">
+        VI. No Warranty, Liability
+      </h3>
+      <p class="block">
+        (1) FZJ does not warrant that the Services will function without errors or interruptions. FZJ reserves
+        the right at any time to change or discontinue any aspect or feature of the offered Services, including,
+        without limitation, the content, hours of availability, and equipment needed for accessing or using the
+        services, as well as to terminate the Services themselves.
+      </p>
+      <p class="block">
+        (2) The Services are provided “as is” without warranty or representation of any kind, either expressed or
+        implied, including, but not limited to, the implied warranties of accuracy, completeness, quality, topicality,
+        fitness for a particular purpose or the absence of any infringement of any proprietary rights of third
+        parties.
+      </p>
+      <p class="block">
+        (3) FZJ is not responsible for the accuracy and correctness of the data.
+        We explicitly don't give any warranty regarding third party Content contributed to the SMS or linked to
+        via SMS and provided on third party websites. The Registered Users as contributors remain solely liable
+        under applicable law for the Content they have contributed to the SMS and for the Content of any URL they
+        link to within the SMS.
+        If applicable, the usage (i.e. rent) of the instruments as indicated in the SMS remains under the
+        conditions that are provided on the respective third party website.
+      </p>
+      <p class="block">
+        (4) Therefore You shall in all cases be entirely and solely liable for the use to which You put such
+        information and material. FZJ shall not be held responsible for the performance of or output obtained
+        from the Services nor for any liability to any party arising out of use of the Services.
+      </p>
+      <p class="block">
+        (5) FZJ shall only be liable for willful misconduct and gross negligence. For slight negligence,
+        FZJ shall only be liable in the event of breach of a material contractual obligation (cardinal obligation),
+        the fulfilment of which is essential for the proper performance of the contract and on the enhancement
+        of which You may regularly rely, as well as in the event of damage resulting from injury to life,
+        limb or health or statutory liability.
+      </p>
+      <p class="block">
+        (6) In no event FZJ shall be liable to You for any incidental, consequential or indirect damages
+        (including, but not limited to, damages for loss of data, cost of procurement of substitute services or
+        service interruptions) arising out of the use or inability to use the Services.
+      </p>
+    </section>
+    <section class="mb-6">
+      <h3 class="mb-1">
+        VII. Applicable Law, Jurisdiction
+      </h3>
+      <p class="block">
+        The Laws of the Federal Republic of Germany apply to these Terms of Use, under exception of the
+        regulations of the Convention of International Sales of Goods.
+      </p>
+      <p class="block">
+        To the extent permitted by law, the District Court of Düren, Germany, shall have jurisdiction with
+        regard to any conflicts between Registered Users, Visitors and us.
+      </p>
+    </section>
+    <section class="mb-6">
+      <h3 class="mb-1">
+        VIII. Adjustment of Clauses, Changes
+      </h3>
+      <p class="block">
+        (1) In case a provision of these Terms of Use is invalid in any way, this does not influence the
+        other provisions of the Terms of Use. The invalid provision will be deemed replaced by a provision
+        that is valid, in view of its original intent and purpose. We will update our Terms of Use as
+        soon as possible.
+      </p>
+      <p class="block">
+        (2) Please be aware that these terms of use may change without notice. In particular, the definition
+        of heavy use may need to be modified in the future and You might have to change your access to those
+        services. Applications using the Services should keep that in mind.
       </p>
     </section>
     <section class="mb-6">
@@ -64,7 +315,7 @@ export default class FzjTermsOfUsePage extends Vue {
   }
 
   get citation (): string {
-    return 'Sensor Management System, https://ibg3sms.ibg.kfa-juelich.de, <date of access>'
+    return 'Sensor Management System, https://sms.earth-data.fz-juelich.de, <date of access>'
   }
 }
 </script>
